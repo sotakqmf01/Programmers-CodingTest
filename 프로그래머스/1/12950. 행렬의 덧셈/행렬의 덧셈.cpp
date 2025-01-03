ac0a75d7e8df233297c6beadd970808202431bca
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-03 10:27:34 +0900

    [level 1] Title: 행렬의 덧셈, Time: 59.88 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -9,11 +9,30 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
     int rows = arr1.size();
     int columns = arr1[0].size();
     
-     for(int i = 0; i<rows; i++){
-        for(int j = 0; j<columns; j++){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
             answer[i][j] = arr1[i][j] + arr2[i][j];
         }
     }
     
+    //vector<vector<int>> answer;
+    
+    //int rows = arr1.size();
+    //int columns = arr1[0].size();
+    
+    //answer.reserve(rows);
+    
+    //for(int i = 0; i<rows; i++){
+        //for(int j = 0; j<columns; j++){
+            // answer는 vector<int>가 여러 개인 vector
+            // => answer[n] 에는 vector<int>가 들어와야함
+            // => 근데 현재 위에서 answer에 reserve로 capacity만 늘려줌
+            // => 밑에 코드는 아무것도 없는 곳에 요소를 넣으려고 하니 segment 에러 발생
+            // ==> 해결 1. 2번째 for문 들어오기 전에 answer[n] 에 vector<int>를 push_back()해서 vector 넣기
+            // ==> 해결 2. resize로 size까지 늘려서 vector<int>를 넣어 주기
+            //answer[i].push_back(arr1[i][j] + arr2[i][j]);
+        //}
+    //}
+        
     return answer;
 }
