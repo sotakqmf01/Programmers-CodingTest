2a83791f8b2e285c3e32c890c2159ee0b71e4b04
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-06 10:03:12 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -4,11 +4,18 @@
 
 using namespace std;
 
+// 유클리드 호제법
+int getGcd(int a, int b){
+    if(b == 0) return a;
+    return getGcd(b, a%b);
+}
+
 vector<int> solution(int n, int m) {
     vector<int> answer;
     
     int num = n < m ? n : m;
     
+    // 연제법제법
     //int gcd_n = n;
     //int gcd_m = m;
     //int gcd = 1;
@@ -33,15 +40,20 @@ vector<int> solution(int n, int m) {
     //int lcm = gcd * gcd_n * gcd_m;
     
     //answer.push_back(gcd);
-    //answer.push_back(lcm); 
+    //answer.push_back(lcm);
     
-    for(int i = num; i > 0; i--){
-        if(n % i == 0 && m % i == 0){
-            answer.push_back(i);
-            answer.push_back(n*m/i);
-            break;
-        }
-    }  
+    //for(int i = num; i > 0; i--){
+    //    if(n % i == 0 && m % i == 0){
+    //        answer.push_back(i);
+    //        answer.push_back(n*m/i);
+    //        break;
+    //    }
+    //}
     
+    int gcd = getGcd(n, m);
+    int lsm = n * m / gcd;
+    answer.push_back(gcd);
+    answer.push_back(lsm);
+        
     return answer;
 }
