0c652d0017c05a46942470689f2568e4c44e5915
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-06 09:42:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -9,31 +9,39 @@ vector<int> solution(int n, int m) {
     
     int num = n < m ? n : m;
     
-    int gcd_n = n;
-    int gcd_m = m;
-    int gcd = 1;
-    int flag = 1;
+    //int gcd_n = n;
+    //int gcd_m = m;
+    //int gcd = 1;
+    //int flag = 1;
     
-    while(flag){
-        for(int i = 2 ; i <= num; i++){
-            if(gcd_n % i == 0 && gcd_m % i == 0){
-                gcd *= i;
-                gcd_n /= i;
-                gcd_m /= i;
-                num /= i;
-                break;
-            }
-            if(i == num)
-                flag = 0;
+    //while(flag){
+    //    for(int i = 2 ; i <= num; i++){
+    //        if(gcd_n % i == 0 && gcd_m % i == 0){
+    //            gcd *= i;
+    //            gcd_n /= i;
+    //            gcd_m /= i;
+    //            num /= i;
+    //            break;
+    //        }
+    //        if(i == num)
+    //            flag = 0;
+    //    }
+    //    if(gcd_n == 1 || gcd_m == 1)
+    //        flag = 0;
+    //}
+    
+    //int lcm = gcd * gcd_n * gcd_m;
+    
+    //answer.push_back(gcd);
+    //answer.push_back(lcm); 
+    
+    for(int i = num; i > 0; i--){
+        if(n % i == 0 && m % i == 0){
+            answer.push_back(i);
+            answer.push_back(n*m/i);
+            break;
         }
-        if(gcd_n == 1 || gcd_m == 1)
-            flag = 0;
-    }
-    
-    int lcm = gcd * gcd_n * gcd_m;
-    
-    answer.push_back(gcd);
-    answer.push_back(lcm);    
+    }  
     
     return answer;
 }
