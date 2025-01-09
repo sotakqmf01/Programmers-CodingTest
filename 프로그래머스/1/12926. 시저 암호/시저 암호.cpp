d1f90a6112fc0316716fcc16bf421d7c1a8ee9f5
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-09 09:51:44 +0900

    [level 1] Title: 시저 암호, Time: 0.10 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    
+    for(int c : s){
+        if(c == ' '){
+            answer += (char)c;
+        }
+        else if(c >= 'A' && c <= 'Z'){
+            c += n;
+            if(c > 'Z')
+                c = c - 'Z' - 1 + 'A';
+            answer += (char)c;
+        }
+        else{
+            // 소문자에서 char형 오버플로우 발생할 수도 있음
+            c += n;
+            if(c > 'z')
+                c = c - 'z' - 1 + 'a';
+            answer += (char)c;
+        }
+    }
+    
+    return answer;
+}
