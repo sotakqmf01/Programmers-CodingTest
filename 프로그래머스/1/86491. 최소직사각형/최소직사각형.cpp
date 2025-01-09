458634f5a19f93d2e26103de9d311d53c35b0381
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-09 09:17:13 +0900

    [level 1] Title: 최소직사각형, Time: 0.99 ms, Memory: 4.64 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+    int size = sizes.size();
+    
+    int w = 0;
+    int h = 0;
+    for(int i = 0; i < size; i++){
+        if(sizes[i][0] < sizes[i][1])
+            swap(sizes[i][0], sizes[i][1]);
+        
+        if(w < sizes[i][0])
+            w = sizes[i][0];
+        if(h < sizes[i][1])
+            h = sizes[i][1];
+    }
+    
+    answer = w * h;
+    
+    return answer;
+}
