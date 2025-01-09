a8d3d33a4886901b8932331e04cc0a5e9b0279e4
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-09 10:25:12 +0900

    [level 1] Title: 시저 암호, Time: 0.08 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -6,22 +6,32 @@ using namespace std;
 string solution(string s, int n) {
     string answer = "";
     
-    for(int c : s){
+    //for(int c : s){
+    //    if(c == ' '){
+    //        answer += (char)c;
+    //    }
+    //    else if(c >= 'A' && c <= 'Z'){
+    //        c += n;
+    //        if(c > 'Z')
+    //            c = c - 'Z' - 1 + 'A';
+    //        answer += (char)c;
+    //    }
+    //    else{
+    //        // 소문자에서 char형 오버플로우 발생할 수도 있음
+    //        c += n;
+    //        if(c > 'z')
+    //            c = c - 'z' - 1 + 'a';
+    //        answer += (char)c;
+    //    }
+    //}
+    
+    for(char& c : s){
         if(c == ' '){
-            answer += (char)c;
-        }
-        else if(c >= 'A' && c <= 'Z'){
-            c += n;
-            if(c > 'Z')
-                c = c - 'Z' - 1 + 'A';
-            answer += (char)c;
+            answer += c;
         }
         else{
-            // 소문자에서 char형 오버플로우 발생할 수도 있음
-            c += n;
-            if(c > 'z')
-                c = c - 'z' - 1 + 'a';
-            answer += (char)c;
+            char alphabet = (c >= 'a') ? 'a' : 'A';
+            answer += alphabet + (char)((c - alphabet + n) % 26);
         }
     }
     
