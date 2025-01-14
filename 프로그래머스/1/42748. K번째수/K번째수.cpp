cf48b89b1d4fb2cef383fd1df2337576d35d2210
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-14 09:43:40 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -4,15 +4,6 @@
 
 using namespace std;
 
-void sort(vector<int>& v, int a, int b){
-    for(int i = a; i < b; i++){
-        for(int j = i; j > a - 1; j--){
-            if(v[j] < v[j-1])
-                swap(v[j], v[j-1]);
-        }
-    }
-}
-
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     int size = commands.size();
@@ -24,8 +15,7 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
         int pick = commands[i][2];
         vector<int> temp = array;
         
-        //sort(temp.begin() + start - 1, temp.end() + last - 1);
-        sort(temp, start, last);
+        sort(temp.begin() + start - 1, temp.begin() + last);
         answer.push_back(temp[start + pick - 2]);
     }
     
