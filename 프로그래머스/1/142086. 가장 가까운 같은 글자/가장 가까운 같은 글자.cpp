b359aaefc48b54621e8109a1d5fef2bf5a1b3d29
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-15 09:41:51 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.25 ms, Memory: 5.14 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -1,28 +1,41 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 vector<int> solution(string s) {
     vector<int> answer;
-    vector<int> alphabet(26, -1);
+    map<char, int> alphabet;
     int i = 0;
     
-    for(char c : s)
-    {
-        if(alphabet[c-'a'] == -1)
-        {
-            // 처음 나오면
+    for(char c : s){
+        if(alphabet.find(c) == alphabet.end())
             answer.push_back(-1);
-        }
         else
-        {
-            // 이전에 나온적이 있으면
-            answer.push_back(i - alphabet[c-'a']);
-        }
-        alphabet[c-'a'] = i;
+            answer.push_back(i - alphabet[c]);
+        alphabet[c] = i;
         i++;
-    }
+    }    
+    
+    //vector<int> alphabet(26, -1);
+    //int i = 0;
+    //
+    //for(char c : s)
+    //{
+    //    if(alphabet[c-'a'] == -1)
+    //    {
+    //        // 처음 나오면
+    //        answer.push_back(-1);
+    //    }
+    //    else
+    //    {
+    //        // 이전에 나온적이 있으면
+    //        answer.push_back(i - alphabet[c-'a']);
+    //    }
+    //    alphabet[c-'a'] = i;
+    //    i++;
+    //}
     
     return answer;
 }
