49181b16fe110518b756d1cd1ccb500562624344
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-19 12:25:46 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -15,32 +15,33 @@ vector<int> solution(int n, int m) {
     
     int num = n < m ? n : m;
     
-    // 연제법
-    //int gcd_n = n;
-    //int gcd_m = m;
-    //int gcd = 1;
-    //int flag = 1;
+    // 연제법(동시에 나눌 수 있는 수로 계속 나누기)
+    int gcd_n = m;
+    int gcd_m = n;
+    int gcd = 1;
+    int flag = 1;
     
-    //while(flag){
-    //    for(int i = 2 ; i <= num; i++){
-    //        if(gcd_n % i == 0 && gcd_m % i == 0){
-    //            gcd *= i;
-    //            gcd_n /= i;
-    //            gcd_m /= i;
-    //            num /= i;
-    //            break;
-    //        }
-    //        if(i == num)
-    //            flag = 0;
-    //    }
-    //    if(gcd_n == 1 || gcd_m == 1)
-    //        flag = 0;
-    //}
+    while(flag){
+        for(int i = 2 ; i <= num; i++){
+            if(gcd_n % i == 0 && gcd_m % i == 0){
+                gcd *= i;
+                gcd_n /= i;
+                gcd_m /= i;
+                num /= i;
+                break;
+            }
+            // 공약수가 없는 경우
+            if(i == num)
+                flag = 0;
+        }
+        if(gcd_n == 1 || gcd_m == 1)
+            flag = 0;
+    }
     
-    //int lcm = gcd * gcd_n * gcd_m;
+    int lcm = gcd * gcd_n * gcd_m;
     
-    //answer.push_back(gcd);
-    //answer.push_back(lcm);
+    answer.push_back(gcd);
+    answer.push_back(lcm);
     
     //for(int i = num; i > 0; i--){
     //    if(n % i == 0 && m % i == 0){
@@ -50,10 +51,10 @@ vector<int> solution(int n, int m) {
     //    }
     //}
     
-    int gcd = getGcd(n, m);
-    int lsm = n * m / gcd;
-    answer.push_back(gcd);
-    answer.push_back(lsm);
+    //int gcd = getGcd(n, m);
+    //int lsm = n * m / gcd;
+    //answer.push_back(gcd);
+    //answer.push_back(lsm);
         
     return answer;
 }
