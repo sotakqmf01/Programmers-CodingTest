a8455e20427f45888c256ba096d674145e865559
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-24 10:40:10 +0900

    [level 1] Title: 옹알이 (2), Time: 0.04 ms, Memory: 3.77 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -7,32 +7,27 @@ int solution(vector<string> babbling) {
     int answer = 0;
     vector<string> nephew = {"aya", "ye", "woo", "ma"};
     
-    for(string babble : babbling)
-    {
+    for(string babble : babbling){
         int NextStringIndex = 0;
         bool isPossible = true;
         int PrevNephewIndex = -1;
         
-        while(NextStringIndex < babble.length() && isPossible)
-        {
-            for(int i = 0; i<nephew.size(); i++)
-            {
-                if(babble.compare(NextStringIndex, nephew[i].length(), nephew[i], 0, nephew[i].length()) == 0)
-                {
-                    if(PrevNephewIndex == i)
-                    {
-                        isPossible = false;
-                    }
-                    else
-                    {
+        // 0. 해당 문자열을 발음할 수 있는지 확인하기 위해 문자열이 끝날 때까지 반복
+        while(NextStringIndex < babble.length() && isPossible){
+            // 1. 발음할 수 있는 각 발음에 대해
+            for(int i = 0; i<nephew.size(); i++){
+                // 1. 같은 길이만큼 문자열과 비교
+                if(babble.compare(NextStringIndex, nephew[i].length(), nephew[i], 0, nephew[i].length()) == 0){
+                    // 2. 연속 발음 못함
+                    if(PrevNephewIndex == i) { isPossible = false; }
+                    else{
                         NextStringIndex += nephew[i].length();
                         PrevNephewIndex = i;
                         isPossible = true;
                         break;
                     }
                 }
-                else
-                {
+                else{
                     isPossible = false;
                 }
             }
