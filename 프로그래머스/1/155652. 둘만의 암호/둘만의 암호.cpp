515a472f7f4d684a7e37d9d2769cf1f6e61f9582
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-30 13:22:36 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -17,12 +17,10 @@ string solution(string s, string skip, int index) {
         int AdditionalIndex = index;
         for(int i = 1; i <= AdditionalIndex; i++)
         {
+            // 알파벳 개수인 26(0~25)에 맞게 index 조정
             int RealIndex = (c-'a'+i <= 25) ? c-'a'+i : (c-'a'+i) % 26;
-            //while(RealIndex > 25)
-            //{
-            //    RealIndex = RealIndex - 26;
-            //}
             
+            // Skip해야할 문자면 for문을 한 번 더 돌게
             if(Alphabet[RealIndex] == -1)
             {
                 AdditionalIndex++;
