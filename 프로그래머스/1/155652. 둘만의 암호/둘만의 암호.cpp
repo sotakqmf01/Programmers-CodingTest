712d21a84fd598c79434df7ffefa18fad5b5d63c
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-30 12:51:05 +0900

    [level 1] Title: 둘만의 암호, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, string skip, int index) {
+    string answer = "";
+    vector<int> Alphabet(26, 1);
+    
+    for(char c : skip)
+    {
+        Alphabet[c-'a'] = -1;
+    }
+    
+    for(char c : s)
+    {
+        int AdditionalIndex = index;
+        for(int i = 1; i <= AdditionalIndex; i++)
+        {
+            int RealIndex = (c-'a'+i <= 25) ? c-'a'+i : c-'a'+i-26;
+            while(RealIndex > 25)
+            {
+                RealIndex = RealIndex - 26;
+            }
+            
+            if(Alphabet[RealIndex] == -1)
+            {
+                AdditionalIndex++;
+            }
+            
+            if(i == AdditionalIndex)
+            {
+                answer += 'a' + RealIndex;
+            }
+        }
+    }
+    
+    
+    return answer;
+}
