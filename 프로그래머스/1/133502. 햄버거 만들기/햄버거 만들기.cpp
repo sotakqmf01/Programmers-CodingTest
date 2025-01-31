8a4618a8fad802c186c73fe091a8f10a611fa9dd
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-31 09:36:25 +0900

    [level 1] Title: 햄버거 만들기, Time: 871.71 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -6,25 +6,21 @@ using namespace std;
 int solution(vector<int> ingredient) {
     int answer = 0;
     
-    for(int i = 0; i <= ingredient.size() - 4; )
+    // size()의 반환값은 size_type(=size_t의 typedef)으로 unsigned int값임
+    // size()가 4보다 작을 경우 ingredient.size() - 4가 음수가 아니라 엄청 큰 정수 값이 됨
+    // => int로 형변환을 해서 원하는 동작을 하도록 해야함
+    for(int i = 0; i <= (int)ingredient.size() - 4; )
     {
-        if(ingredient.size() >= 4)
+        if(ingredient[i] == 1 && ingredient[i+1] == 2 && ingredient[i+2] == 3 && ingredient[i+3] == 1)
         {
-            if(ingredient[i] == 1 && ingredient[i+1] == 2 && ingredient[i+2] == 3 && ingredient[i+3] == 1)
-            {
-                ++answer;
-                vector<int>::iterator it = ingredient.begin() + i;
-                ingredient.erase(it, it+4);
-                i = (i - 3 >= 0) ? i - 3 : 0;
-            }
-            else
-            {
-                ++i;
-            }
+            ++answer;
+            vector<int>::iterator it = ingredient.begin() + i;
+            ingredient.erase(it, it+4);
+            i = (i - 3 >= 0) ? i - 3 : 0;
         }
         else
         {
-            break;
+            ++i;
         }
     }
     
