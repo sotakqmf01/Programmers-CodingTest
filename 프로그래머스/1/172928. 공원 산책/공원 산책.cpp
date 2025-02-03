dd20aa0242835ebc0c60b9970b21fd3c4744520f
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-02-03 10:23:07 +0900

    [level 1] Title: 공원 산책, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
@@ -0,0 +1,96 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<string> park, vector<string> routes) {
+    vector<int> answer;
+    int ParkHeight = park.size();
+    int ParkWidth = park[0].size();
+    int StartX = 0;
+    int StartY = 0;
+    
+    for(int i = 0; i < ParkHeight; i++){
+        for(int j = 0; j < ParkWidth; j++){
+            if(park[i][j] == 'S'){
+                StartX = j;
+                StartY = i;
+            }
+        }
+    }
+    
+    for(const string& route : routes)
+    {
+        char direction = route[0];
+        int move = route[2] - '0';
+        bool CanGo = true;
+
+        
+        if(direction == 'E'){
+            for(int i = 1; i <= move; i++){
+                if(StartX + i >= ParkWidth){
+                    CanGo = false;
+                    break;
+                }
+                if(park[StartY][StartX + i] == 'X'){
+                    CanGo = false;
+                    break;
+                }
+            }
+            if(CanGo){
+                StartX = StartX + move;
+            }
+        }
+        else if(direction == 'W'){
+            for(int i = 1; i <= move; i++){
+                if(StartX - i < 0){
+                    CanGo = false;
+                    break;
+                }
+                if(park[StartY][StartX - i] == 'X'){
+                    CanGo = false;
+                    break;
+                }
+            }
+            if(CanGo){
+                StartX = StartX - move;
+            }
+        }
+        else if(direction == 'S'){
+            for(int i = 1; i <= move; i++){
+                if(StartY + i >= ParkHeight){
+                    CanGo = false;
+                    break;
+                }
+                if(park[StartY + i][StartX] == 'X'){;
+                    CanGo = false;
+                    break;
+                }
+            }
+            if(CanGo){
+                StartY = StartY + move;
+            }
+        }
+        else if(direction == 'N'){
+            for(int i = 1; i <= move; i++){
+                if(StartY - i < 0){
+                    CanGo = false;
+                    break;
+                }
+                if(park[StartY - i][StartX] == 'X'){
+                    CanGo = false;
+                    break;
+                }
+            }
+            if(CanGo){
+                StartY = StartY - move;
+            }
+        }
+    }
+    
+    answer.push_back(StartY);
+    answer.push_back(StartX);
+    
+    return answer;
+}
