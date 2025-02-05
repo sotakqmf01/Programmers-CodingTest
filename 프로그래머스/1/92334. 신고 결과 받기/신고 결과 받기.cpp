4471ac0e3b2d5556124c751b6f0011e06ecee6eb
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-02-05 10:26:28 +0900

    [level 1] Title: 신고 결과 받기, Time: 192.43 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -9,7 +9,7 @@ using namespace std;
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer(id_list.size(), 0);
     map<string, int> UserIndex;
-    // 각 유저별로 자신을 신고한 user들의 이름을 저장할 벡터
+    // 각 유저별로 [자신을 신고한 유저들의 이름을 저장]할 벡터
     vector<set<string>> User(id_list.size());
     
     for(int i = 0; i < id_list.size(); i++)
@@ -23,22 +23,22 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
         int blankPos = str.find(" ");
         string reportingUser = str.substr(0, blankPos);
         string reportedUser = str.substr(blankPos + 1);
-                
+        
+        // 자신을 신고한 유저 추가
         User[UserIndex[reportedUser]].insert(reportingUser);
     }
     
     for(int i = 0; i < User.size(); i++)
     {
-        //cout << User[i].size();
         if(User[i].size() >= k)
         {
+            // 신고한 유저들에게 메일 발송
             for(const string& reportingUser : User[i])
             {
                 answer[UserIndex[reportingUser]]++;
             }
         }
     }
-    
     
     return answer;
 }
