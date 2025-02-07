e9831de23c741ac9e697fe7c68e9c4cc33f177cc
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-02-07 10:41:25 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -7,17 +7,30 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
+    //string numStr;
     long long num = 0;
     long long min = LLONG_MAX;
     long long max = LLONG_MIN;
+    //string::size_type sz;
     
     istringstream iss(s);
-    
+
+    //while(getline(iss, numStr, ' '))
     while(iss >> num)
     {
+        //num = stoll(numStr);
         min = num < min ? num : min;
         max = num > max ? num : max;
     }
+    
+    //while(!s.empty())
+    //{
+    //    num = stoll(s, &sz);
+    //    min = num < min ? num : min;
+    //    max = num > max ? num : max;
+    //    if(s.length() > sz) s = s.substr(sz);
+    //    else s = "";        
+    //}
     
     answer = to_string(min) + " " + to_string(max);
     
