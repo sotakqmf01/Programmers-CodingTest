d2d1fc3de8bf6cdeae7ef955f11c9efc7471782b
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-02-17 10:57:29 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.80 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -3,28 +3,31 @@
 
 using namespace std;
 
+// 괄호 문자열이 올바른지 확인
 bool CheckCorrect(string s)
 {
-    string CheckS = "";
+    string CheckS = ""; // stack 사용
     
+    // 괄호 문자열이 올바른지 확인하려면 가장 안쪽 괄호부터 체크해야 함
+    // ex) [([)]] - X / [([])] - O
     for(char c : s)
     {
+        // 여는 괄호는 그냥 추가
         if(c == '[' || c == '{' || c == '(')
-        {
             CheckS += c;
-        }
         else
         {
-            if(CheckS.back() == '[' && c == ']')
+            // 닫는 괄호는 문자열 마지막 괄호와 비교
+            if(CheckS.empty())
+                return false;
+            else if(CheckS.back() == '[' && c == ']')
                 CheckS.erase(CheckS.length() - 1);
             else if(CheckS.back() == '{' && c == '}')
                 CheckS.erase(CheckS.length() - 1);
             else if(CheckS.back() == '(' && c == ')')
                 CheckS.erase(CheckS.length() - 1);
             else
-            {
                 return false;
-            }
         }
     }
     
@@ -37,11 +40,13 @@ bool CheckCorrect(string s)
 int solution(string s) {
     int answer = 0;
     
+    // 괄호 문자열 회전
     for(int i = 0; i < s.length(); i++)
     {
         char c = s[0];
-        s.erase(s.begin());
+        s.erase(s.begin()); 
         s += c;
+        // s = s.substr(1) + s[0];
         
         if(CheckCorrect(s))
             answer++;
