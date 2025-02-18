099c7571d89fa6785035dbc4307852dfa3f3d0ba
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-02-18 09:50:43 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3562.06 ms, Memory: 23.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+void SequenceSum(vector<int> elements, int n, set<int>& SeqSum)
+{
+    // 첫번째 요소부터 마지막 요소까지
+    for(int i = 0; i < elements.size(); i++)
+    {
+        int sum = 0;
+        
+        // 각 요소에서 길이가 n인 연속 부분 수열의 합
+        for(int j = 0; j < n; j++)
+        {
+            sum += elements[(i + j) % elements.size()];
+        }
+        
+        SeqSum.insert(sum);
+    }
+}
+
+int solution(vector<int> elements) {
+    int answer = 0;
+    set<int> SeqSum;
+    
+    // 길이 1 ~ n
+    for(int i = 1; i <= elements.size(); i++)
+    {
+        SequenceSum(elements, i, SeqSum);
+    }
+    
+    return answer = SeqSum.size();
+}
