a84c724d337de25cd15b80598425b57e88f63709
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-02-20 09:46:19 +0900

    [level 2] Title: n^2 배열 자르기, Time: 43.22 ms, Memory: 24.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    
+    // 1. 그림 그려보니까 arr[x][y]의 값이 x와 y 둘 중 큰 값에 +1 한 것(index는 0부터 시작하니까)
+    // 2. 거기에 left와 right를 2차원 좌표로 변환하면 해당 위치의 값을 알 수 있겠다고 생각
+    //    - 각각의 좌표는 (x = left / n), (y = left % n) 로 구할 수 있음
+    // => 그럼 그냥 left ~ right까지 2번 작업을 하고 1번 작업을 하면 1차원 배열 바로 만들 수 있겠다고 생각
+    for(long long i = left; i <= right; i++)
+    {
+        if(i/n > i%n)
+        {
+            answer.push_back(i/n + 1);
+        }
+        else
+        {
+            answer.push_back(i%n + 1);
+        }
+    }
+    
+    return answer;
+}
