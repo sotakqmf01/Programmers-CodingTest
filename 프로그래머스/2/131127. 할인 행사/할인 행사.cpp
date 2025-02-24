f0d500b4acdcfc48d273787ea1f02ad07fb6fd78
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-02-24 10:10:46 +0900

    [level 2] Title: 할인 행사, Time: 19.77 ms, Memory: 12 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+    unordered_map<string, int> umap;
+    
+    // 첫 할인 품목 10개 중 각 제품의 개수
+    for(int i = 0; i < 10; i++)
+    {
+        umap[discount[i]]++;
+    }
+        
+    // 첫날 ~ n-10째날까지 열흘에 대해 체크
+    for(int i = 0; i <= discount.size()-10; i++)
+    {
+        // 제품 수가 같은 경우
+        if(umap.size() == want.size())
+        {
+            bool bIsMembership = true;
+            
+            // 원하는 제품들이 원하는 개수만큼 있는지
+            for(int j = 0; j < umap.size(); j++)
+            {
+                if(umap[want[j]] != number[j])
+                {
+                    bIsMembership = false;
+                    break;
+                }
+            }
+            
+            if(bIsMembership)
+            {
+                answer++;
+            }
+        }
+        
+        if(i == discount.size()-10)
+        {
+            break;
+        }
+        
+        // umap을 다음날에 대한 열흘치 품목으로 최신화
+        if(umap[discount[i]] == 1)
+        {
+            umap.erase(discount[i]);
+            umap[discount[i+10]]++;
+        }
+        else
+        {
+            umap[discount[i]]--;
+            umap[discount[i+10]]++;
+        }
+    }
+    
+    return answer;
+}
