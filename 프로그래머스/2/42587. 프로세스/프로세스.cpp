a2a48e4b8470849eb40b9882499bb878022dc20a
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-03 20:08:15 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,46 +1,94 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
-int solution(vector<int> priorities, int location) {
-    int Count = 0;  // 실행한 프로세스 수
-    int CurrentProcessIndex = max_element(priorities.begin(), priorities.end()) - priorities.begin();
+// queue의 첫 요소부터 우선순위를 비교해보면서 문제 설명 과정 반복
+int solution(vector<int> priorities, int location){
+    int Count = 0;
+    queue<pair<int, int>> WaitingQueue;
+    int HighestPriority = 0;
     
-    // 반례입니다.
-    // 입력값 〉 [2, 3, 1, 2], 3
-    // 기댓값 〉 2
-    // => (웬만하면 대기 큐에서 순서대로 가져와야함) - 순서대로 가져오는건 당연함 큐에 들어있는 거니까
-    while(Count < priorities.size())
+    // 제일 높은 우선 순위 찾기 + queue 초기화
+    for(int i = 0; i < priorities.size(); i++)
     {
-        Count++;
-        priorities[CurrentProcessIndex] = -1;
+        WaitingQueue.push({priorities[i], i});
         
-        if(CurrentProcessIndex == location)
+        if(HighestPriority < priorities[i])
         {
-            return Count;
+            HighestPriority = priorities[i];
         }
-        
-        // 현재 인덱스 기준[]으로 이전/이후() 큐에 있는 것 비교하기 위해
-        // {(2,2,3,7,3),[9],(2,3,6)} => 이전 큐에 있는 7을 다음에 실행
-        // {(2,2,3,6,3),[9],(2,3,6)} => 이후 큐에 있는 6을 다음에 실행
-        int BeforeHighestPriority = *max_element(priorities.begin(), priorities.begin() + CurrentProcessIndex);
-        int AfterHighestPriority = *max_element(priorities.begin() + CurrentProcessIndex, priorities.end());
-        if(BeforeHighestPriority > AfterHighestPriority)
+    }
+    
+    // 큐에서 하나씩 꺼내보기
+    while(!WaitingQueue.empty())
+    {
+        // 가장 높은 우선순위를 가진 프로세스인 경우
+        if(WaitingQueue.front().first >= HighestPriority)
         {
-            CurrentProcessIndex = max_element(priorities.begin(), priorities.begin() + CurrentProcessIndex) - priorities.begin();
+            Count++;
+            if(WaitingQueue.front().second == location)
+            {
+                return Count;
+            }
+            
+            // 다음으로 높은 우선 순위 찾기
+            priorities[WaitingQueue.front().second] = -1;
+            HighestPriority = *max_element(priorities.begin(), priorities.end());            
+            
+            WaitingQueue.pop();
         }
+        // 그 외
         else
         {
-            // 웬만하면 대기 큐에서 순서대로 가져와야함
-             CurrentProcessIndex = max_element(priorities.begin() + CurrentProcessIndex, priorities.end()) - priorities.begin();
+            WaitingQueue.push(WaitingQueue.front());
+            WaitingQueue.pop();
         }
     }
     
     return Count;
 }
 
+//int solution(vector<int> priorities, int location) {
+//    int Count = 0;  // 실행한 프로세스 수
+//    int CurrentProcessIndex = max_element(priorities.begin(), priorities.end()) - priorities.begin();
+//    
+//    // 반례입니다.
+//    // 입력값 〉 [2, 3, 1, 2], 3
+//    // 기댓값 〉 2
+//    // => (웬만하면 대기 큐에서 순서대로 가져와야함) - 순서대로 가져오는건 당연함 큐에 들어있는 거니까
+//    while(Count < priorities.size())
+//    {
+//        Count++;
+//        priorities[CurrentProcessIndex] = -1;
+//        
+//        if(CurrentProcessIndex == location)
+//        {
+//            return Count;
+//        }
+//        
+//        // 현재 인덱스 기준[]으로 이전/이후() 큐에 있는 것 비교하기 위해
+//        // {(2,2,3,7,3),[9],(2,3,6)} => 이전 큐에 있는 7을 다음에 실행
+//        // {(2,2,3,6,3),[9],(2,3,6)} => 이후 큐에 있는 6을 다음에 실행
+//        int BeforeHighestPriority = *max_element(priorities.begin(), priorities.begin() + CurrentProcessIndex);
+//        int AfterHighestPriority = *max_element(priorities.begin() + CurrentProcessIndex, priorities.end());
+//        if(BeforeHighestPriority > AfterHighestPriority)
+//        {
+//            CurrentProcessIndex = max_element(priorities.begin(), priorities.begin() + CurrentProcessIndex) - priorities.begin();
+//        }
+//        else
+//        {
+//            // 웬만하면 대기 큐에서 순서대로 가져와야함
+//             CurrentProcessIndex = max_element(priorities.begin() + CurrentProcessIndex, priorities.end()) - priorities.begin();
+//        }
+//    }
+//    
+//    return Count;
+//}
+
+// 오답
 //int solution(vector<int> priorities, int location) {
 //    // 반례입니다.
 //    // 입력값 〉 [2, 3, 1, 2], 3
