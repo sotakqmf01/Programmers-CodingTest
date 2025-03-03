80a1ae189ebc6032f489e03d14dea70782ee9651
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-03 18:13:42 +0900

    [level 2] Title: 프로세스, Time: 0.04 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -11,7 +11,7 @@ int solution(vector<int> priorities, int location) {
     // 반례입니다.
     // 입력값 〉 [2, 3, 1, 2], 3
     // 기댓값 〉 2
-    // => (웬만하면 대기 큐에서 순서대로 가져와야함)
+    // => (웬만하면 대기 큐에서 순서대로 가져와야함) - 순서대로 가져오는건 당연함 큐에 들어있는 거니까
     while(Count < priorities.size())
     {
         Count++;
@@ -39,52 +39,52 @@ int solution(vector<int> priorities, int location) {
     }
     
     return Count;
-    
-    // 반례입니다.
-    // 입력값 〉 [2, 3, 1, 2], 3
-    // 기댓값 〉 2
-    // => (웬만하면 대기 큐에서 순서대로 가져와야함)
-    //int Count = 0;  // 실행한 프로세스 수
-    //int CurrentProcessIndex = max_element(priorities.begin(), priorities.end()) - priorities.begin();
-    //int NextProcessIndex;
-    //
-    //if(CurrentProcessIndex == priorities.size() - 1)
-    //{
-    //    NextProcessIndex = 0;
-    //}
-    //else
-    //{
-    //    NextProcessIndex = CurrentProcessIndex + 1;
-    //}
-    //
-    //while(Count < priorities.size())
-    //{
-    //    Count++;
-    //    priorities[CurrentProcessIndex] = -1;
-    //    
-    //    if(CurrentProcessIndex == location)
-    //    {
-    //        return Count;
-    //    }
-    //    
-    //    if(*max_element(priorities.begin(), priorities.end()) > priorities[NextProcessIndex])
-    //    {
-    //        CurrentProcessIndex = max_element(priorities.begin(), priorities.end()) - priorities.begin();
-    //    }
-    //    else
-    //    {
-    //        CurrentProcessIndex = NextProcessIndex;
-    //    }
-    //    
-    //    if(CurrentProcessIndex == priorities.size() - 1)
-    //    {
-    //        NextProcessIndex = 0;
-    //    }
-    //    else
-    //    {
-    //        NextProcessIndex = CurrentProcessIndex + 1;
-    //    }
-    //}
-    //
-    //return Count;
 }
+
+//int solution(vector<int> priorities, int location) {
+//    // 반례입니다.
+//    // 입력값 〉 [2, 3, 1, 2], 3
+//    // 기댓값 〉 2
+//    // => (웬만하면 대기 큐에서 순서대로 가져와야함)
+//    int Count = 0;  // 실행한 프로세스 수
+//    int CurrentProcessIndex = max_element(priorities.begin(), priorities.end()) - priorities.begin();
+//    int NextProcessIndex;
+//    
+//    if(CurrentProcessIndex == priorities.size() - 1)
+//    {
+//        NextProcessIndex = 0;
+//    }
+//    else
+//    {
+//        NextProcessIndex = CurrentProcessIndex + 1;
+//    }
+//    
+//    while(Count < priorities.size())
+//    {
+//        Count++;
+//        // 실행한 프로세스는 우선순위 -1로 설정해서 다음 프로세스 찾을 때 문제가 없도록 함
+//        priorities[CurrentProcessIndex] = -1;
+//        
+//        if(CurrentProcessIndex == location)
+//        {
+//            return Count;
+//        }
+//        
+//        // 다음으로 높은 우선순위를 갖는 프로세스 찾기
+//        // {(1,1),[9],(1,1,1)} 이라면 9(2번 프로세스)가 실행되고나면, 다음에 1(3번 프로세스)가 실행되어야 함
+//        // ▶ 여기서 순서대로해야하는걸 생각했는데 예외는 생각을 못했네...
+//        if(*max_element(priorities.begin(), priorities.end()) > priorities[NextProcessIndex])
+//        {
+//            CurrentProcessIndex = max_element(priorities.begin(), priorities.end()) - priorities.begin();
+//        }
+//        else
+//        {
+//            CurrentProcessIndex = NextProcessIndex;
+//        }
+//        
+//        // 우선 순위가 높은 프로세스의 Index의 다음 Index
+//        NextProcessIndex = (CurrentProcessIndex + 1) % priorites.size() ;
+//    }
+//    
+//    return Count;
+//}
