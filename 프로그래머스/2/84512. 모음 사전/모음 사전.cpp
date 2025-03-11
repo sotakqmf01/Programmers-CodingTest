0cbf481188f7ca534af406f0e67ca7451c298e41
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-11 11:23:55 +0900

    [level 2] Title: 모음 사전, Time: 0.11 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -3,19 +3,31 @@
 
 using namespace std;
 
+// 수학적인 풀이도 가능함 생각해보고 해보기
+
 int answer = 0;
 bool bFind = false;
 
+// 단어의 길이가 최대 5 => depth 필요
+// 현재 단어 => checkWord
+// 다음 단어를 위한 알파벳 배열 => vector
+// 찾는 단어 => word
 void DFS(int depth, string checkWord, const vector<char>& alphabet, string word)
 {
-    answer++;
+    // 알파벳이 빈 문자열이 아닌 경우에만
+    if(checkWord != "")
+    {
+        answer++;
+    }
     
+    // 찾은 경우에는 더 이상 DFS를 실행하지 않도록 
     if(checkWord == word)
     {
         bFind = true;
         return;
     }
 
+    // 문자의 길이는 최대 5
     if(depth < 5)
     {
         for(int i = 0; i < alphabet.size(); i++)
@@ -33,6 +45,5 @@ int solution(string word) {
     
     DFS(0, checkWord, alphabet, word);
 
-    // 첫 DFS 탐색(문자열에 아무것도 없는 경우)을 빼야함
-    return answer-1;
+    return answer;
 }
