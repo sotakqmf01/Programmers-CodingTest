cf3deeece3d2f1b903d168c06e4cd3fcee85463a
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-12 12:41:59 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 394.68 ms, Memory: 242 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -4,72 +4,40 @@
 
 using namespace std;
 
-#include <string>
-#include <vector>
-#include <stack>
-
-using namespace std;
-
+// stack 사용 - DP와 비슷한 로직
 vector<int> solution(vector<int> numbers) {
-    vector<int> answer(numbers.size(), -1);
-    stack<int> stk;
-
-    for(int i=0; i<numbers.size(); i++) {
-        while(!stk.empty() && numbers[stk.top()] < numbers[i]) {
-            answer[stk.top()] = numbers[i];
-            stk.pop();
+    vector<int> answer(numbers.size(), -1); // answer는 뒷 큰수가 있는지 없는지 알 수 있음
+    stack<int> st;          // 스택에는 다음 수와 answer(다음 수의 뒷 큰수)를 넣음
+    
+    st.push(numbers[numbers.size()-1]);
+    
+    // 배열의 뒤에서부터 체크
+    for(int i = numbers.size() - 2; i >= 0; i--)
+    {        
+        while(!st.empty())
+        {
+            int num = st.top();
+            st.pop();
+            
+            if(numbers[i] < num)
+            {
+                answer[i] = num;
+                break;
+            }
         }
-        stk.push(i);
+        
+        // 뒷 큰수가 있으면, 뒷 큰수까지 비교해볼 기회가 있어야함
+        // - {[6], 5, 4, 4, 4, 7} 인 경우
+        // - 뒷 큰수가 없으면 number[i]가 현재까지 가장 큰 수이니까 뒷 큰수는 몰라도 됨
+        if(answer[i] != -1)
+        {
+            st.push(answer[i]);    
+        }
+        st.push(numbers[i]);
     }
-
+    
     return answer;
 }
-
-//테스트 14 〉	통과 (169.88ms, 121MB)
-//테스트 15 〉	통과 (362.42ms, 240MB)
-//테스트 16 〉	통과 (367.68ms, 240MB)
-//테스트 17 〉	통과 (362.63ms, 240MB)
-//테스트 18 〉	통과 (337.65ms, 240MB)
-//테스트 19 〉	통과 (351.61ms, 240MB)
-//테스트 20 〉	통과 (346.53ms, 238MB)
-//테스트 21 〉	통과 (394.68ms, 242MB)
-//테스트 22 〉	통과 (265.23ms, 191MB)
-//테스트 23 〉	통과 (348.83ms, 240MB)
-
-// stack 사용 - DP와 비슷한 로직
-//vector<int> solution(vector<int> numbers) {
-//    vector<int> answer(numbers.size(), -1); // answer는 뒷 큰수가 있는지 없는지 알 수 있음
-//    stack<int> st;          // 스택에는 다음 수와 answer(다음 수의 뒷 큰수)를 넣음
-//    
-//    st.push(numbers[numbers.size()-1]);
-//    
-//    // 배열의 뒤에서부터 체크
-//    for(int i = numbers.size() - 2; i >= 0; i--)
-//    {        
-//        while(!st.empty())
-//        {
-//            int num = st.top();
-//            st.pop();
-//            
-//            if(numbers[i] < num)
-//            {
-//                answer[i] = num;
-//                break;
-//            }
-//        }
-//        
-//        // 뒷 큰수가 있으면, 뒷 큰수까지 비교해볼 기회가 있어야함
-//        // - {[6], 5, 4, 4, 4, 7} 인 경우
-//        // - 뒷 큰수가 없으면 number[i]가 현재까지 가장 큰 수이니까 뒷 큰수는 몰라도 됨
-//        if(answer[i] != -1)
-//        {
-//            st.push(answer[i]);    
-//        }
-//        st.push(numbers[i]);
-//    }
-//    
-//    return answer;
-//}
 
 // DP - 이중 for문 - answer배열을 메모이제이션으로 사용
 //vector<int> solution(vector<int> numbers) {
