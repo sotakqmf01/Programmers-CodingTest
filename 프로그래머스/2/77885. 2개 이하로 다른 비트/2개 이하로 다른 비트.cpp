c850cd67f0a736c47132ba30059880928c313fe9
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-18 10:12:05 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.52 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -10,11 +10,18 @@ vector<long long> solution(vector<long long> numbers) {
     {
         long long check = 1;
         long long prevCheck = 0;
+        
+        // f(7) = 11 예제를 보면 규칙을 찾을 수 있음
+        // => 첫번째 비트(제일 오른쪽 비트)부터 비교했을 때 0이면 그냥 바꾸면 됨
+        // ==> 근데 1이 나오고 0(01, 0111)이 나오는 경우에는 0을 1로 바꾸고 0 바로 뒤에 있는 1을 0으로 바꿔야함
         while(1)
         {
+            // 해당 자리 비트가 0이면
             if(!(number & check))
             {
-                number = number | check;
+                // 0을 1로 바꾸고
+                number = number + check;
+                // 0 뒤에 1이였으면, 1을 0으로
                 if(prevCheck != 0)
                 {
                     number = number - prevCheck;
@@ -22,11 +29,13 @@ vector<long long> solution(vector<long long> numbers) {
                 answer.push_back(number);
                 break;
             }
+            // 해당 자리 비트가 1이면 그 위치를 기억
             else
             {
                 prevCheck = check;
             }
             
+            // 비트 자리 이동
             check = check << 1;
         }
     }
