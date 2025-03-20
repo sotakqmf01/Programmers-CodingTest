8ac717e4a737e8a1f2872e0730dee8c02059a5c9
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-20 10:45:39 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.23 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -9,6 +9,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     queue<int> q;
     int currentWeight = 0;
     int acrossTruckCount = 0;
+    int waitTruckCount = truck_weights.size();
     int nextTruck = 0;
     
     // 모든 트럭이 건너갈 동안 1초에 한 칸 씩 이동
@@ -17,9 +18,11 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
         // 한 칸 씩 이동
         answer++;
         
+        // queue를 다리라고 보고
         if(q.size() == bridge_length)
         {
             int truckWeight = q.front();
+            // 양수는 트럭, 음수는 빈 자리
             if(truckWeight > 0)
             {
                 currentWeight -= truckWeight;
@@ -37,7 +40,9 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
             break;
         }
         
-        if(currentWeight + truck_weights[nextTruck] <= weight)
+        // 기다리는 트럭이 있는 경우에만 다리에 올라갈 수 있는지 비교해보고
+        // 그 외에는 빈 자리 넣기
+        if(waitTruckCount != 0 && currentWeight + truck_weights[nextTruck] <= weight)
         {
             currentWeight += truck_weights[nextTruck];
             q.push(truck_weights[nextTruck]);
@@ -45,6 +50,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
             {
                 nextTruck++;
             }
+            waitTruckCount--;
         }
         else
         {
