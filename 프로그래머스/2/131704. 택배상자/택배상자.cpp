507e7e0b08d435cc430fcd91959771218d684092
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-30 14:12:46 +0900

    [level 2] Title: 택배상자, Time: 16.27 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.cpp
+++ 프로그래머스/2/131704. 택배상자/택배상자.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// 코드 길이를 좀 더 줄일 수 있는 법 생각해보기
 int solution(vector<int> order) {
     int answer = 0;
     queue<int> mainBelt;
@@ -30,12 +31,14 @@ int solution(vector<int> order) {
             subBox = subBelt.top();
         }
         
+        // 메인 벨트의 상자와 배달 순서 상자 비교
         if(mainBox == order[orderIndex])
         {
             mainBelt.pop();
             answer++;
             orderIndex++;
         }
+        // 보조 벨트의 상자와 배달 순서 상자 비교
         else if(subBox == order[orderIndex])
         {
             subBelt.pop();
@@ -44,11 +47,13 @@ int solution(vector<int> order) {
         }
         else
         {
+            // 메인 벨트의 상자를 보조 벨트로 옮기기
             if(!mainBelt.empty())
             {
                 subBelt.push(mainBox);
                 mainBelt.pop();
             }
+            // 처리할 수 있는 상자가 없는 경우
             else
             {
                 break;
