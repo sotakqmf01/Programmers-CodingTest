61902274481676f8446b617ae1d7887062f8ccf7
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-03-30 14:13:23 +0900

    [level 2] Title: 택배상자, Time: 10.92 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.cpp
+++ 프로그래머스/2/131704. 택배상자/택배상자.cpp
@@ -4,62 +4,78 @@
 #include <stack>
 
 using namespace std;
-
-// 코드 길이를 좀 더 줄일 수 있는 법 생각해보기
 int solution(vector<int> order) {
     int answer = 0;
-    queue<int> mainBelt;
-    stack<int> subBelt;
-    
-    for(int i = 1; i <= order.size(); i++)
+    stack<int> st;
+    for(int i = 1; i <= order.size(); ++i)
     {
-        mainBelt.push(i);
-    }
-    
-    int orderIndex = 0;
-    while(!mainBelt.empty() || !subBelt.empty())
-    {
-        int mainBox = 0;
-        int subBox = 0;
-        
-        if(!mainBelt.empty())
+        st.push(i);
+
+        while(!st.empty() && order[answer] == st.top())
         {
-            mainBox = mainBelt.front();
-        }
-        if(!subBelt.empty())
-        {
-            subBox = subBelt.top();
-        }
-        
-        // 메인 벨트의 상자와 배달 순서 상자 비교
-        if(mainBox == order[orderIndex])
-        {
-            mainBelt.pop();
+            st.pop();
             answer++;
-            orderIndex++;
-        }
-        // 보조 벨트의 상자와 배달 순서 상자 비교
-        else if(subBox == order[orderIndex])
-        {
-            subBelt.pop();
-            answer++;
-            orderIndex++;
-        }
-        else
-        {
-            // 메인 벨트의 상자를 보조 벨트로 옮기기
-            if(!mainBelt.empty())
-            {
-                subBelt.push(mainBox);
-                mainBelt.pop();
-            }
-            // 처리할 수 있는 상자가 없는 경우
-            else
-            {
-                break;
-            }
         }
     }
-    
+
     return answer;
 }
+
+// 코드 길이를 좀 더 줄일 수 있는 법 생각해보기
+//int solution(vector<int> order) {
+//    int answer = 0;
+//    queue<int> mainBelt;
+//    stack<int> subBelt;
+//    
+//    for(int i = 1; i <= order.size(); i++)
+//    {
+//        mainBelt.push(i);
+//    }
+//    
+//    int orderIndex = 0;
+//    while(!mainBelt.empty() || !subBelt.empty())
+//    {
+//        int mainBox = 0;
+//        int subBox = 0;
+//        
+//        if(!mainBelt.empty())
+//        {
+//            mainBox = mainBelt.front();
+//        }
+//        if(!subBelt.empty())
+//        {
+//            subBox = subBelt.top();
+//        }
+//        
+//        // 메인 벨트의 상자와 배달 순서 상자 비교
+//        if(mainBox == order[orderIndex])
+//        {
+//            mainBelt.pop();
+//            answer++;
+//            orderIndex++;
+//        }
+//        // 보조 벨트의 상자와 배달 순서 상자 비교
+//        else if(subBox == order[orderIndex])
+//        {
+//            subBelt.pop();
+//            answer++;
+//            orderIndex++;
+//        }
+//        else
+//        {
+//            // 메인 벨트의 상자를 보조 벨트로 옮기기
+//            if(!mainBelt.empty())
+//            {
+//                subBelt.push(mainBox);
+//                mainBelt.pop();
+//            }
+//            // 처리할 수 있는 상자가 없는 경우
+//            else
+//            {
+//                break;
+//            }
+//        }
+//    }
+//    
+//    return answer;
+//}
