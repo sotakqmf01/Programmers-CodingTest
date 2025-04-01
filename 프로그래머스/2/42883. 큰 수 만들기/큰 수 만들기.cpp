43e6703a279d926f904f7ea313205d2aedced891
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-04-01 11:45:56 +0900

    [level 2] Title: 큰 수 만들기, Time: 6.12 ms, Memory: 5.58 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+// 뭔가 뒤 큰 수 문제가 생각이 나네
+// https://school.programmers.co.kr/learn/courses/30/lessons/154539
 string solution(string number, int k) {
     string answer = "";
     stack<int> compareStack;
@@ -52,3 +54,22 @@ string solution(string number, int k) {
     
     return answer;
 }
+
+// 다른 사람 풀이임
+// i ~ i+k(총 k개) 내의 최댓값의 위치를 찾고, 그 위치까지 거리를 구해서 k에서 빼고 해당 위치까지의 숫자 삭제
+// ex) 12345, 3
+//  - idx [0,3) => 1,2,3 중 3이 최대
+//  - 3까지 거리는 2 => k = k - 2 = 1;
+//  - 해당 위치까지 숫자 지우기 => 345
+//string solution(string number, int k) {
+//    for (int i = 0; i < number.size() - k; ++i) {
+//        auto iter = std::max_element(number.begin() + i, number.begin() + i + k + 1);
+//        if (iter != number.begin() + i) {
+//            k = k - std::distance(number.begin() + i, iter);
+//            number.erase(number.begin() + i, iter);
+//        }
+//    }
+//  - 이후 k가 남아있으면(내림차순인 경우), 오른쪽에서 k개 자름
+//    number.erase(number.end() - k, number.end());
+//    return number;
+//}
