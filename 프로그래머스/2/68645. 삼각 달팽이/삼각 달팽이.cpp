4ae1dfed4d7c4358672713e6b40570850e78b773
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-04-07 10:13:18 +0900

    [level 2] Title: 삼각 달팽이, Time: 182.13 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -0,0 +1,73 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum DIR{
+    DOWN = 0,
+    RIGHT = 1,
+    UP = 2
+};
+
+vector<vector<int>> triSnail;
+int g_n;
+
+bool CanGo(int x, int y)
+{
+    if(x < 0 || x >= g_n)
+        return false;
+    if(y < 0 || y > x)
+        return false;
+    if(triSnail[x][y] > 0)
+        return false;
+    return true;
+}
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    int totalNum = n * (n+1) / 2;
+    int num = 1;
+    int x = 0, y = 0;
+    int dir = DOWN;
+    int dx[] = {1, 0, -1};  // down, right, up
+    int dy[] = {0, 1, -1};  // down, right, up
+    
+    g_n = n;
+    triSnail.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+    	triSnail[i] = vector<int>(i + 1, -1);
+    }
+    
+    while(num <= totalNum)
+    {  
+        triSnail[x][y] = num;
+        
+        if (num == totalNum)
+		break;
+        
+        int nextX = x + dx[dir];
+        int nextY = y + dy[dir];
+        
+        if(CanGo(nextX, nextY))
+        {
+            x = nextX;
+            y = nextY;
+            num++;
+        }
+        else
+        {
+            dir = (dir + 1) % 3;
+        }
+    }
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < triSnail[i].size(); j++)
+        {
+            answer.push_back(triSnail[i][j]);
+        }
+    }
+    
+    return answer;
+}
