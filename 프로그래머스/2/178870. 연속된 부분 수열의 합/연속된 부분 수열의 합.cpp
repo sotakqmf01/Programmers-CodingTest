c26539fba8bab26797598f1c9ab875c3403db8e2
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-04-21 10:52:17 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 7.27 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 vector<int> solution(vector<int> sequence, int k) {
     vector<int> answer;
+    // 굳이 queue를 사용하지 않아도 될 것 같았는데,
+    // 뭔가 로직을 생각하고 나니 선입선출의 모양이라 사용
     queue<int> q;
     int firstIndex = 0;
     int index = 0;
@@ -15,9 +17,12 @@ vector<int> solution(vector<int> sequence, int k) {
         q.push(num);
         sum += num;
         
+        // if랑 else 부분은 필요 없는 코드
+        // else if 부분을 그냥 if로 해도 될 듯
         if(sum < k){
             continue;
         }
+        // 합이 k보다 크면 작아질 때까지 앞 요소 빼기
         else if(sum > k){
             while(sum > k)
             {
@@ -31,6 +36,7 @@ vector<int> solution(vector<int> sequence, int k) {
         else{}
         
         if(sum == k){
+            // 부분 수열의 길이 비교
             if(length > q.size())
             {
                 firstIndex = index;
@@ -45,11 +51,12 @@ vector<int> solution(vector<int> sequence, int k) {
     return answer;
 }
 
+// 다른 사람 풀이 - 투포인터
 //vector<int> solution(vector<int> sequence, int k)
 //{
 //    vector<int> answer;
 //
-//    int srt=0, end=0, sum=0;
+//    int start=0, end=0, sum=0;
 //
 //    for(int i=0; i<sequence.size(); i++)
 //    {
@@ -57,11 +64,11 @@ vector<int> solution(vector<int> sequence, int k) {
 //        sum+=sequence[end];
 //
 //        while(sum>k)
-//            sum-=sequence[srt++];
+//            sum-=sequence[start++];
 //
 //        if(sum==k)
-//            if(answer.empty() || end-srt < answer[1]-answer[0])
-//                answer={srt, end};
+//            if(answer.empty() || end-start < answer[1]-answer[0])
+//                answer={start, end};
 //    }
 //
 //    return answer;
