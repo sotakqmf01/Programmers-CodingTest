b92a43f71ae6c6a80d526f7963b37fc23fc8709a
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-04-25 10:58:14 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 3.45 ms, Memory: 7.28 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.cpp
@@ -69,61 +69,47 @@ using namespace std;
 //    return answer;
 //}
 
-int rotate(vector<vector<int>>& matrix, pair<int, int> query_start, pair<int, int> query_end)
-{
-    int first = matrix[query_start.first - 1][query_start.second - 1];
-    int min_val = first;
-
-    for (int i = query_start.first; i < query_end.first; i++)
-    {
-        matrix[i - 1][query_start.second - 1] = matrix[i][query_start.second - 1];
-
-        min_val = min(min_val, matrix[i - 1][query_start.second - 1]);
-    }
-
-    for (int i = query_start.second; i < query_end.second; i++)
-    {
-        matrix[query_end.first - 1][i - 1] = matrix[query_end.first - 1][i];
-
-        min_val = min(min_val, matrix[query_end.first - 1][i - 1]);
-    }
-
-    for (int i = query_end.first - 2; i >= query_start.first - 1; i--)
-    {
-        matrix[i + 1][query_end.second - 1] = matrix[i][query_end.second - 1];
-
-        min_val = min(min_val, matrix[i + 1][query_end.second - 1]);
-    }
-
-    for (int i = query_end.second - 2; i > query_start.second - 1; i--)
-    {
-        matrix[query_start.first - 1][i + 1] = matrix[query_start.first - 1][i];
-
-        min_val = min(min_val, matrix[query_start.first - 1][i + 1]);
-    }
-
-    matrix[query_start.first - 1][query_start.second] = first;
-
-    return min_val;
-}
-
+// 다른 사람 풀이
 vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
     vector<int> answer;
-    vector<vector<int>> matrix(rows, vector<int>(columns));
+    int field[101][101];
 
-    int num = 1;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            matrix[i][j] = num++;
+    for (int i = 1 ; i <= rows ; i++){
+        for (int j = 1 ; j <= columns ; j++){
+            field[i][j] = (i - 1) * columns + j;
         }
     }
 
-    for (auto query : queries)
-    {
-        answer.push_back(rotate(matrix, { query[0], query[1] }, { query[2], query[3] }));
+    for (int i = 0 ; i < (int)queries.size() ; i++){
+        int mini = field[queries[i][0]][queries[i][1]];
+        int temp = field[queries[i][0]][queries[i][1]];
+        
+        // UP
+        for (int j = queries[i][0] ; j < queries[i][2] ; j++){
+            field[j][queries[i][1]] = field[j + 1][queries[i][1]];
+            mini = min(mini, field[j][queries[i][1]]);
+        }
+        // LEFT
+        for (int j = queries[i][1] ; j < queries[i][3] ; j++){
+            field[queries[i][2]][j] = field[queries[i][2]][j + 1];
+            mini = min(mini, field[queries[i][2]][j]);
+        }
+        // DOWN
+        for (int j = queries[i][2] ; j > queries[i][0] ; j--){
+            field[j][queries[i][3]] = field[j - 1][queries[i][3]];
+            mini = min(mini, field[j][queries[i][3]]);
+        }
+        // RIGHT(first 제외)
+        for (int j = queries[i][3] ; j > queries[i][1] ; j--){
+            field[queries[i][0]][j] = field[queries[i][0]][j - 1];
+            mini = min(mini, field[queries[i][0]][j]);
+        }
+
+        field[queries[i][0]][queries[i][1] + 1] = temp;
+
+        answer.push_back(mini);
     }
 
     return answer;
 }
+
