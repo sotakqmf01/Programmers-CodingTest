3a2a8b55c46c27921b5e4864825d9627f1c3d724
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-05-10 18:10:28 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 166.16 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+// Y = (w/h)X 그래프보다 밑에 있는 격자의 개수 세는 방식
+// - 다만 큰 수의 나눗셈을 할 때 float를 사용하면 부동 소수점 정밀도가 떨어져, 오차가 클 수 있음
+// - double을 쓰면 조금이나마 해결할 수 있지만, 근본적인 해결은 아님
 long long solution(int w,int h) {
     long long answer = 0;
     
@@ -19,10 +22,10 @@ long long solution(int w,int h) {
 //    long long answer = w * h;
 //    
 //    for(int x = 1; x <= h; x++){
-//        float currY = (float)w / h * x;
-//        float prevY = (float)w / h * (x-1);
+//        double currY = (double)w / h * x;
+//        double prevY = (double)w / h * (x-1);
 //        
-//        int uselessCount = ceil(currY) - floor(prevY);
+//        int uselessCount = (int)ceil(currY) - (int)floor(prevY);
 //        answer -= uselessCount;
 //    }
 //    
