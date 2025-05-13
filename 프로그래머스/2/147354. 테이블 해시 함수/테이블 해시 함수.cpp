88e3a55cc0b8739d8b84cb75699d06a282a90a53
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-05-13 09:56:49 +0900

    [level 2] Title: 테이블 해시 함수, Time: 5.42 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -1,30 +1,29 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 int standardKey = 0;
 
-//bool compare(const vector<int>& tuple1, const vector<int>& tuple2){
-//    if(tuple1[standardKey - 1] == tuple2[standardKey - 1]){
-//        return tuple1[0] > tuple2[0];
-//    }
-//    
-//    return tuple1[standardKey -1] < tuple2[standardKey - 1];
-//}
+bool compare(const vector<int>& tuple1, const vector<int>& tuple2){
+    if(tuple1[standardKey - 1] == tuple2[standardKey - 1]){
+        return tuple1[0] > tuple2[0];
+    }
+    
+    return tuple1[standardKey -1] < tuple2[standardKey - 1];
+}
 
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
     int S_i = 0;
     int hash = 0;
     standardKey = col;
     
-    //sort(data.begin(), data.end(), compare);
+    sort(data.begin(), data.end(), compare);
     
-    sort(data.begin(), data.end(), [col](const vector<int>& tuple1, const vector<int>& tuple2){
-        return (tuple1[col - 1] == tuple2[col - 1]) ? (tuple1[0] > tuple2[0]) : (tuple1[col - 1] < tuple2[col - 1]);
-    });
+    //sort(data.begin(), data.end(), [col](const vector<int>& tuple1, const vector<int>& tuple2){
+    //    return (tuple1[col - 1] == tuple2[col - 1]) ? (tuple1[0] > tuple2[0]) : (tuple1[col - 1] < tuple2[col - 1]);
+    //});
         
     for(int i = row_begin - 1; i < row_end; i++){
         S_i = 0;
