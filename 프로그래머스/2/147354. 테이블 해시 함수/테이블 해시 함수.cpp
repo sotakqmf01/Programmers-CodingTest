9502da77878412717b29c3ea002be4146c582b0d
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-05-13 09:49:35 +0900

    [level 2] Title: 테이블 해시 함수, Time: 12.13 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int standardKey = 0;
-
+//
 bool compare(vector<int> tuple1, vector<int> tuple2){
     if(tuple1[standardKey - 1] == tuple2[standardKey - 1]){
         return tuple1[0] > tuple2[0];
@@ -14,6 +14,42 @@ bool compare(vector<int> tuple1, vector<int> tuple2){
     
     return tuple1[standardKey -1] < tuple2[standardKey - 1];
 }
+//
+//int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+//    int S_i = 0;
+//    int hash = 0;
+//    standardKey = col;
+//    
+//    sort(data.begin(), data.end(), compare);
+//        
+//    for(int i = row_begin - 1; i < row_end; i++){
+//        S_i = 0;
+//        
+//        for(int j = 0; j < data[i].size(); j++){
+//            S_i += data[i][j] % (i+1);
+//        }
+//        
+//        hash ^= S_i;
+//    }
+//    
+//    return hash;
+//}
+
+
+//bool compare(vector<int> tuple1, vector<int> tuple2){
+//    if(tuple1[standardKey - 1] < tuple2[standardKey - 1]){
+//        return true;
+//    }
+//    else if(tuple1[standardKey - 1] > tuple2[standardKey - 1]){
+//        return false;
+//    }
+//    else{
+//        if(tuple1[0] > tuple1[0])
+//            return true;
+//        else
+//            return false;
+//    }
+//}
 
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
     int S_i = 0;
@@ -21,7 +57,7 @@ int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
     standardKey = col;
     
     sort(data.begin(), data.end(), compare);
-        
+    
     for(int i = row_begin - 1; i < row_end; i++){
         S_i = 0;
         
@@ -29,7 +65,10 @@ int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
             S_i += data[i][j] % (i+1);
         }
         
-        hash ^= S_i;
+        if(i == row_begin - 1)
+            hash = S_i;
+        else
+            hash = hash ^ S_i;
     }
     
     return hash;
