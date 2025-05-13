5fc429c7356240a925f2294bde5113409e7e16ae
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-05-13 09:44:19 +0900

    [level 2] Title: 테이블 해시 함수, Time: 9.34 ms, Memory: 57.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int standardKey = 0;
+
+bool compare(vector<int> tuple1, vector<int> tuple2){
+    if(tuple1[standardKey - 1] == tuple2[standardKey - 1]){
+        return tuple1[0] > tuple2[0];
+    }
+    
+    return tuple1[standardKey -1] < tuple2[standardKey - 1];
+}
+
+int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+    int S_i = 0;
+    int hash = 0;
+    standardKey = col;
+    
+    sort(data.begin(), data.end(), compare);
+        
+    for(int i = row_begin - 1; i < row_end; i++){
+        S_i = 0;
+        
+        for(int j = 0; j < data[i].size(); j++){
+            S_i += data[i][j] % (i+1);
+        }
+        
+        hash ^= S_i;
+    }
+    
+    return hash;
+}
