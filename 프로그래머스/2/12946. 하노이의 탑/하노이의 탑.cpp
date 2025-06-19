93bed2fb62ae3d3f4b1fb601592a85423941e6f5
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-06-19 22:02:17 +0900

    [level 2] Title: 하노이의 탑, Time: 28.93 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 void Hanoi(int Plate, int Src, int Mid, int Dst, vector<vector<int>>& Ways){
-    // 1. 원판이 1개인 경우 : 그냥 목표 기둥으로 이동
+    // 1. 원판이 1개인 경우 : 그냥 목표 기둥으로 옮기기
     if(Plate == 1){
-        Ways.push_back(vector({Src, Dst}));
-
+        Ways.push_back(vector<int>({Src, Dst}));
+        
         return;
     }
     
@@ -15,11 +15,11 @@ void Hanoi(int Plate, int Src, int Mid, int Dst, vector<vector<int>>& Ways){
     // - 2.1. 가장 큰 원판을 제외한 나머지 원판들 보조 기둥으로 옮기기
     Hanoi(Plate - 1, Src, Dst, Mid, Ways);
     
-    // - 2.2. 남아있는 가장 큰 원판을 목표 기둥으로 이동
-    Ways.push_back(vector({Src, Dst}));
+    // - 2.2. 남아있는 가장 큰 원판을 목표 기둥으로 옮기기
+    Ways.push_back(vector<int>({Src, Dst}));
     
-    // - 2.3. 보조 기둥에 있는 것을 목표 기둥으로 이동
-    Hanoi(Plate - 1, Mid, Src, Dst, Ways);
+    // - 2.3. 보조 기둥으로 옮긴 원판들을 목표 기둥으로 옮기기
+    Hanoi(Plate - 1, Mid, Src, Dst, Ways);    
 }
 
 vector<vector<int>> solution(int n) {
@@ -29,3 +29,59 @@ vector<vector<int>> solution(int n) {
     
     return answer;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int GetEmptyListIndex(const vector<list<int>>& Hanoi){
+//    
+//}
+//
+//vector<vector<int>> solution(int n) {
+//    vector<vector<int>> answer;
+//    vector<list<int>> Hanoi(3, list<int>());
+//    
+//    for(int i = n; n > 0; i--){
+//        Hanoi[0].push_back(i);    
+//    }
+//    
+//    int plate = Hanoi[0].back();
+//    Hanoi[0].pop_back();
+//    
+//    if(n % 2 == 0){
+//        Hanoi[1].push_back(plate);
+//        answer.push_back({1, 2});
+//    }
+//    else{
+//        Hanoi[2].push_back(plate);
+//        answer.push_back({1, 3});
+//    }
+//    
+//    while(Hanoi[2].size() != n){
+//        int emptyIndex = GetEmptyListIndex(Hanoi);
+//        int maxIndex = GetMaxIndex
+//        if(emptyIndex != -1){
+//            int maxIndex
+//        }
+//    }
+//    
+//    return answer;
+//}
