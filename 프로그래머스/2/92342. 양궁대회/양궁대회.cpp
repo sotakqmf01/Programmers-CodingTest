9653fb6e01666b7ca58853603b4e94ded712c42c
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-04 13:12:46 +0900

    [level 2] Title: 양궁대회, Time: 8.53 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -4,80 +4,80 @@
 using namespace std;
 
 // 비트 마스킹을 사용한 풀이도 해보기
-//int ScoreDiff(const vector<int>& info, const vector<int>& temp){
-//    // 점수 계산
-//    int apitch = 0;
-//    int lion = 0;
-//    for(int i = 0; i<info.size(); i++){
-//        if(info[i] > temp[i]){
-//            apitch += info.size() - 1 - i;
-//        }
-//        else if(info[i] < temp[i]){
-//            lion += info.size() - 1 - i;
-//        }
-//        else{
-//            if(info[i] != 0)
-//                apitch += info.size() - 1 - i;
-//        }
-//    }
-//    
-//    return lion - apitch;
-//}
-//
-//bool TieBreak(const vector<int>& answer, const vector<int>& temp){
-//    for(int i = answer.size() - 1; i >= 0; i--){
-//        if(answer[i] != temp[i])
-//            return answer[i] < temp[i];
-//    }
-//    
-//    return false;
-//}
-//
-//void CompareScore(const vector<int>& info, vector<int>& answer, vector<int>& temp){
-//    int tempScoreDiff = ScoreDiff(info, temp);
-//    if(tempScoreDiff <= 0)
-//        return;
-//    
-//    int answerScoreDiff = ScoreDiff(info, answer);
-//    if(tempScoreDiff > answerScoreDiff || (tempScoreDiff == answerScoreDiff && TieBreak(answer, temp)))
-//        answer = temp;
-//}
-//
-//void dfs(int depth, int n, const vector<int>& info, vector<int>& answer, vector<int>& temp){
-//    if(n == 0){
-//        CompareScore(info, answer, temp);
-//        return;
-//    }
-//    
-//    if(depth == info.size() - 1){
-//        if (n > 0){
-//            // ★★백트래킹
-//            temp[info.size() - 1] += n;
-//            CompareScore(info, answer, temp);
-//            temp[info.size() - 1] -= n;
-//        }
-//        return;
-//    }
-//    
-//    for(int i = 0; i <= n; i++){
-//        // ★백트래킹 - 분배형
-//        temp[depth] = i;
-//        dfs(depth + 1, n - i, info, answer, temp);
-//        temp[depth] = 0;
-//    }
-//}
-//
-//vector<int> solution(int n, vector<int> info) {
-//    vector<int> answer((int)info.size(), 0);
-//    vector<int> temp((int)info.size(), 0);
-//    
-//    dfs(0, n, info, answer, temp);
-//    
-//    if(ScoreDiff(info, answer) <= 0)
-//        return vector<int>{-1};
-//    
-//    return answer;
-//}
+int ScoreDiff(const vector<int>& info, const vector<int>& temp){
+    // 점수 계산
+    int apitch = 0;
+    int lion = 0;
+    for(int i = 0; i<info.size(); i++){
+        if(info[i] > temp[i]){
+            apitch += info.size() - 1 - i;
+        }
+        else if(info[i] < temp[i]){
+            lion += info.size() - 1 - i;
+        }
+        else{
+            if(info[i] != 0)
+                apitch += info.size() - 1 - i;
+        }
+    }
+    
+    return lion - apitch;
+}
+
+bool TieBreak(const vector<int>& answer, const vector<int>& temp){
+    for(int i = answer.size() - 1; i >= 0; i--){
+        if(answer[i] != temp[i])
+            return answer[i] < temp[i];
+    }
+    
+    return false;
+}
+
+void CompareScore(const vector<int>& info, vector<int>& answer, vector<int>& temp){
+    int tempScoreDiff = ScoreDiff(info, temp);
+    if(tempScoreDiff <= 0)
+        return;
+    
+    int answerScoreDiff = ScoreDiff(info, answer);
+    if(tempScoreDiff > answerScoreDiff || (tempScoreDiff == answerScoreDiff && TieBreak(answer, temp)))
+        answer = temp;
+}
+
+void dfs(int depth, int n, const vector<int>& info, vector<int>& answer, vector<int>& temp){
+    if(n == 0){
+        CompareScore(info, answer, temp);
+        return;
+    }
+    
+    if(depth == info.size() - 1){
+        if (n > 0){
+            // ★★백트래킹
+            temp[info.size() - 1] += n;
+            CompareScore(info, answer, temp);
+            temp[info.size() - 1] -= n;
+        }
+        return;
+    }
+    
+    for(int i = 0; i <= n; i++){
+        // ★백트래킹 - 분배형
+        temp[depth] = i;
+        dfs(depth + 1, n - i, info, answer, temp);
+        temp[depth] = 0;
+    }
+}
+
+vector<int> solution(int n, vector<int> info) {
+    vector<int> answer((int)info.size(), 0);
+    vector<int> temp((int)info.size(), 0);
+    
+    dfs(0, n, info, answer, temp);
+    
+    if(ScoreDiff(info, answer) <= 0)
+        return vector<int>{-1};
+    
+    return answer;
+}
 
 // 다른 사람 풀이
 //int N, ans;
@@ -119,91 +119,77 @@ using namespace std;
 //    return answer;
 //}
 
-#include <functional>
-
-vector<int> solution(int n, vector<int> info) {
-    vector<vector<int>> answers;
-    int ryan = 0, apeach = 0;
-
-    for (int i = 0; i < info.size(); ++i)
-    {
-        if (0 < info[i])
-        {
-            apeach += 10 - i;
-        }
-    }
-
-    vector<int> counts(11, 0);
-    function<void(int, int, int)> dfs = [&](int idx, int rest, int sum) {
-
-        if (0 == rest)
-        {
-            if (ryan <= sum)
-            {
-                if (ryan < sum)
-                {
-                    answers.clear();
-                }
-                ryan = sum;
-                answers.push_back(counts);
-            }
-            return;
-        }
-
-        int promising = (10 - idx) * (11 - idx) / 2 * 2;
-        if (0 < 10 - idx - rest)
-        {
-            promising -= (10 - idx - rest) * (11 - idx - rest) / 2 * 2;
-        }
-        if (sum + promising <= apeach || sum + promising < ryan)
-        {
-            return;
-        }
-
-        if (10 > idx && 0 < rest - info[idx])
-        {
-            counts[idx] = info[idx] + 1;
-            int tmp = 1 < counts[idx] ? sum + (10 - idx) * 2 : sum + (10 - idx);
-            dfs(idx + 1, rest - counts[idx], tmp);
-        }
-
-        if (10 == idx)
-        {
-            counts[idx] = rest;
-        }
-        else
-        {
-            counts[idx] = 0;
-        }
-        dfs(idx + 1, rest - counts[idx], sum);
-        counts[idx] = 0;
-    };
-
-    dfs(0, n, 0);
-
-    if (apeach >= ryan)
-    {
-        return { -1 };
-    }
-    else
-    {
-        int answer = 0, max1 = 0, max2 = 0;
-        for (int i = 0; i < answers.size(); ++i)
-        {
-            for (int j = answers[i].size() - 1; j >= 0; --j)
-            {
-                if (0 < answers[i][j])
-                {
-                    if (max1 < j || (max1 == j && max2 < answers[i][j]))
-                    {
-                        answer = i;
-                        max1 = j;
-                        max2 = answers[i][j];
-                        break;
-                    }
-                }
-            }
-        }
-        return answers[answer];
-    }
-}
+// 질문하기 - 다른 사람 풀이
+//#include <functional>
+//
+//vector<int> solution(int n, vector<int> info) {
+//    vector<vector<int>> answers;
+//    int ryan = 0, apeach = 0;
+//
+//    for (int i = 0; i < info.size(); ++i){
+//        if (0 < info[i]){
+//            apeach += 10 - i;
+//        }
+//    }
+//
+//    vector<int> counts(11, 0);
+//    function<void(int, int, int)> dfs = 
+//        [&](int idx, int rest, int sum) {
+//            if (0 == rest){
+//                if (ryan <= sum){
+//                    if (ryan < sum){
+//                        answers.clear();
+//                    }
+//                    ryan = sum;
+//                    answers.push_back(counts);
+//                }
+//                return;
+//            }
+//    
+//            int promising = (10 - idx) * (11 - idx) / 2 * 2;
+//            if (0 < 10 - idx - rest){
+//                promising -= (10 - idx - rest) * (11 - idx - rest) / 2 * 2;
+//            }
+//            if (sum + promising <= apeach || sum + promising < ryan){
+//                return;
+//            }
+//    
+//            if (10 > idx && 0 < rest - info[idx]){
+//                counts[idx] = info[idx] + 1;
+//                int tmp = 1 < counts[idx] ? sum + (10 - idx) * 2 : sum + (10 - idx);
+//                dfs(idx + 1, rest - counts[idx], tmp);
+//            }
+//    
+//            if (10 == idx){
+//                counts[idx] = rest;
+//            }
+//            else{
+//                counts[idx] = 0;
+//            }
+//            dfs(idx + 1, rest - counts[idx], sum);
+//            counts[idx] = 0;
+//        };
+//
+//    dfs(0, n, 0);
+//
+//    if (apeach >= ryan){
+//        return { -1 };
+//    }
+//    else
+//    {
+//        int answer = 0, max1 = 0, max2 = 0;
+//        for (int i = 0; i < answers.size(); ++i){
+//            for (int j = answers[i].size() - 1; j >= 0; --j){
+//                if (0 < answers[i][j]){
+//                    if (max1 < j || (max1 == j && max2 < answers[i][j])){
+//                        answer = i;
+//                        max1 = j;
+//                        max2 = answers[i][j];
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        return answers[answer];
+//    }
+//}
