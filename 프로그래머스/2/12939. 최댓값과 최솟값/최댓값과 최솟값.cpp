8f87886fcb7f1c6302e960f4d450177846bc794a
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-23 14:08:28 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.59 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,38 +1,57 @@
 #include <string>
 #include <vector>
 #include <limits.h>
+#include <algorithm>
 #include <sstream>
 
 using namespace std;
 
-string solution(string s) {
+//string solution(string s) {
+//    string answer = "";
+//    //string numStr;
+//    long long num = 0;
+//    long long min = LLONG_MAX;
+//    long long max = LLONG_MIN;
+//    //string::size_type sz;
+//    
+//    istringstream iss(s);
+//
+//    //while(getline(iss, numStr, ' '))
+//    while(iss >> num)
+//    {
+//        //num = stoll(numStr);
+//        min = num < min ? num : min;
+//        max = num > max ? num : max;
+//    }
+//    
+//    //while(!s.empty())
+//    //{
+//    //    num = stoll(s, &sz);
+//    //    min = num < min ? num : min;
+//    //    max = num > max ? num : max;
+//    //    if(s.length() > sz) s = s.substr(sz);
+//    //    else s = "";        
+//    //}
+//    
+//    answer = to_string(min) + " " + to_string(max);
+//    
+//    return answer;
+//}
+
+string solution(string s){
     string answer = "";
-    //string numStr;
-    long long num = 0;
-    long long min = LLONG_MAX;
-    long long max = LLONG_MIN;
-    //string::size_type sz;
+    long long minValue = LLONG_MAX;
+    long long maxValue = LLONG_MIN;
     
     istringstream iss(s);
-
-    //while(getline(iss, numStr, ' '))
-    while(iss >> num)
-    {
-        //num = stoll(numStr);
-        min = num < min ? num : min;
-        max = num > max ? num : max;
+    long long num;
+    
+    while(iss >> num){
+        minValue = min(minValue, num);
+        maxValue = max(maxValue, num);
     }
     
-    //while(!s.empty())
-    //{
-    //    num = stoll(s, &sz);
-    //    min = num < min ? num : min;
-    //    max = num > max ? num : max;
-    //    if(s.length() > sz) s = s.substr(sz);
-    //    else s = "";        
-    //}
-    
-    answer = to_string(min) + " " + to_string(max);
+    answer = to_string(minValue) + " " + to_string(maxValue);
     
     return answer;
 }
