814aa663d64a8e5f867b2a3db4b7eb79814972b8
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-23 16:14:08 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 1.28 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -6,9 +6,8 @@ using namespace std;
 //int solution(int n) {
 //    int answer = 0;
 //    
-//    for(int i = 1; i<n; i++){
-//        if(n%i == 1)
-//        {
+//    for(int i = 2; i < n; i++){
+//        if(n % i == 1){
 //            answer = i;
 //            break;
 //        }
@@ -17,15 +16,24 @@ using namespace std;
 //    return answer;
 //}
 
+// 다른 사람 풀이
 int solution(int n){
-    int x;
+    int answer;
     
-    for(int i = 2; i < n; i++){
-        if(n % i == 1){
-            x = i;
-            break;
-        }
-    }
+    if (n&1) return 2;
+    else for(int i=3; i<n; i+=2) if(n%i==1) return i;
     
-    return x;
+    // 홀수이면, 무조건 2
+    //if(n % 2 == 1)
+    //    answer = 2;
+    //
+    //// 짝수이면, 홀수들 중에서 찾아야함
+    //for(int i = 3; i < n; i += 2){
+    //    if(n % i == 1){
+    //        answer = i;
+    //        break;
+    //    }
+    //}
+    
+    return answer;
 }
