fbbf9a538138f3851e8a5f15ddbdab40ce138564
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-23 15:32:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -1,38 +1,60 @@
 #include <string>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
+//string solution(string s) {
+//    string answer = "";
+//    string Word = "";
+//    
+//    // 다른 사람 풀이 보기
+//    for(char c : s)
+//    {
+//        if(c == ' ')
+//        {
+//            if(!Word.empty())
+//            {
+//                Word[0] = toupper(Word[0]);
+//                answer += Word;
+//                Word = "";
+//            }
+//            answer += c;
+//        }
+//        else
+//        {
+//            Word += tolower(c);
+//        }
+//    }
+//    
+//    if(!Word.empty())
+//    {
+//        Word[0] = toupper(Word[0]);
+//        answer += Word;
+//        Word = "";
+//    }
+//        
+//    return answer;
+//}
+
+
 string solution(string s) {
     string answer = "";
-    string Word = "";
+    string word = "";
     
-    // 다른 사람 풀이 보기
-    for(char c : s)
-    {
-        if(c == ' ')
-        {
-            if(!Word.empty())
-            {
-                Word[0] = toupper(Word[0]);
-                answer += Word;
-                Word = "";
-            }
+    for(char c : s){
+        if(c != ' '){
+            word += tolower(c);
+        }
+        else{
+            word[0] = toupper(word[0]);
+            answer += word;
+            word = "";
             answer += c;
         }
-        else
-        {
-            Word += tolower(c);
-        }
     }
     
-    if(!Word.empty())
-    {
-        Word[0] = toupper(Word[0]);
-        answer += Word;
-        Word = "";
-    }
-        
+    word[0] = toupper(word[0]);
+    answer += word;
+    
     return answer;
 }
