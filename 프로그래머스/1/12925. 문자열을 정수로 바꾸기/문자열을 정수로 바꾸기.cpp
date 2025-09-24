5846cfa3090f184ec23807ed7f045bf5d809834b
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 17:22:40 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -3,45 +3,45 @@
 
 using namespace std;
 
-//int solution(string s) {
-//    int answer = 0;
-//    int flag = 1;
-//    int i = 0;
-//    
-//    if(s[0] == '+'){
-//        flag = 1;
-//        i = 1;
-//    }
-//    if(s[0] == '-'){
-//        flag = -1;
-//        i = 1;
-//    }
-//    for(i; i<s.length(); i++){
-//            answer = answer*10 + (s[i]-'0');  
-//    }
-//    
-//    answer *= flag;
-//    
-//    return answer;
-//}
-
-int solution(string s){
+int solution(string s) {
     int answer = 0;
     int flag = 1;
+    int i = 0;
     
-    for(const char& ch : s){
-        if(ch == '+')
-            flag = 1;
-        else if(ch == '-')
-            flag = -1;
-        else
-            answer = answer * 10 + (ch - '0');
+    if(s[0] == '+'){
+        flag = 1;
+        i = 1;
+    }
+    if(s[0] == '-'){
+        flag = -1;
+        i = 1;
+    }
+    for(i; i<s.length(); i++){
+            answer = answer*10 + (s[i]-'0');  
     }
     
     answer *= flag;
     
     return answer;
 }
+
+//int solution(string s){
+//    int answer = 0;
+//    int flag = 1;
+//    
+//    for(const char& ch : s){
+//        if(ch == '+')
+//            flag = 1;
+//        else if(ch == '-')
+//            flag = -1;
+//        else
+//            answer = answer * 10 + (ch - '0');
+//    }
+//    
+//    answer *= flag;
+//    
+//    return answer;
+//}
 
 //int solution(string s){
 //    return stoi(s);
