7bfc9d8b44a5f944869c8bffb8f7e539aa740422
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 15:35:55 +0900

    [level 2] Title: 피보나치 수, Time: 0.87 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -3,15 +3,26 @@
 
 using namespace std;
 
-vector<long long> Fibonacci = {0, 1};
+//vector<long long> Fibonacci = {0, 1};
+//
+//int solution(int n) {
+//    int answer = 0;
+//    
+//    for(int i = 0; i <= n - 2; i++)
+//    {
+//        Fibonacci.push_back((Fibonacci[i] + Fibonacci[i+1]) % 1234567);
+//    }
+//   
+//    return Fibonacci[n];
+//}
 
-int solution(int n) {
-    int answer = 0;
+int solution(int n){
+    vector<long long> fibonacci(n+1, 0);
+    fibonacci[1] = 1;
     
-    for(int i = 0; i <= n - 2; i++)
-    {
-        Fibonacci.push_back((Fibonacci[i] + Fibonacci[i+1]) % 1234567);
+    for(int i = 2; i <= n; i++){
+        fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]) % 1234567;
     }
-   
-    return Fibonacci[n];
+        
+    return fibonacci[n];
 }
