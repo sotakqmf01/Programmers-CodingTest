5b9853bed8bbd202262f5415180dbc97c63aea9e
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 17:31:04 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 4.11 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -16,23 +16,23 @@ using namespace std;
 //}
 
 // 시간 초과
-//long long solution(long long n){
-//    long long answer = -1;
-//    
-//    for(int i = 1; i*i <= n; i++){
-//        if(i * i == n)
-//            answer = (i + 1) * (i + 1);
-//    }
-//    
-//    return answer;
-//}
-
 long long solution(long long n){
     long long answer = -1;
     
-    long long root = (long long)floor(sqrt(n));
-    if(root * root == n)
-        answer = (root + 1) * (root + 1);
+    for(long long i = 1; i*i <= n; i++){
+        if(i * i == n)
+            answer = (i + 1) * (i + 1);
+    }
     
     return answer;
 }
+
+//long long solution(long long n){
+//    long long answer = -1;
+//    
+//    long long root = (long long)floor(sqrt(n));
+//    if(root * root == n)
+//        answer = (root + 1) * (root + 1);
+//    
+//    return answer;
+//}
