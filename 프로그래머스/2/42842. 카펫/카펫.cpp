633f666564f0a4e3e80789c5c8d1167c2796e358
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 19:24:22 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -4,21 +4,38 @@
 
 using namespace std;
 
-vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
+//vector<int> solution(int brown, int yellow) {
+//    vector<int> answer;
+//    
+//    for(int i = 1; i <= sqrt(yellow); i++)
+//    {
+//        if(yellow % i == 0)
+//        {
+//            int j = yellow / i;
+//            if(((j + 1) * 2 + (i + 1) * 2) == brown)
+//            {
+//                answer.push_back(j+2);
+//                answer.push_back(i+2);
+//            }
+//        }
+//    }
+//    
+//    return answer;
+//}
+
+vector<int> solution(int brown, int yellow){
+    vector<int> answer(2, 0);
+    int total = brown + yellow;
     
-    for(int i = 1; i <= sqrt(yellow); i++)
-    {
-        if(yellow % i == 0)
-        {
-            int j = yellow / i;
-            if(((j + 1) * 2 + (i + 1) * 2) == brown)
-            {
-                answer.push_back(j+2);
-                answer.push_back(i+2);
+    for(int i = 3; i*i <= total; i++){
+        if(total % i == 0){
+            if(2*(i + (total / i)) - 4 == brown){
+                answer[0] = total / i;
+                answer[1] = i;
+                break;
             }
         }
     }
-    
+        
     return answer;
 }
