85940c77347dc1e46524534b38ab3fcf427066bd
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 17:46:43 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -1,44 +1,45 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//long long solution(long long n) {
-//    long long answer = 0;
-//
-//    vector<int> v(1, n%10);
-//    
-//    for(int i=0; n/=10; i++){
-//        v.push_back(n%10);
-//    }
-//    
-//    for(int i = 1; i < v.size(); i ++){
-//        for(int j = i; j > 0; j--){
-//            if(v[j-1] < v[j]){
-//                int temp = v[j];
-//                v[j] = v[j-1];
-//                v[j-1] = temp;
-//            } 
-//        }
-//    }
-//    
-//    for(int i = 0; i < v.size(); i++){
-//        answer = answer * 10 + v[i];
-//    }
-//    
-//    return answer;
-//}
-
-#include <algorithm>
-
-long long solution(long long n){
+long long solution(long long n) {
     long long answer = 0;
+
+    vector<int> v(1, n%10);
     
-    string s = to_string(n);
-    sort(s.begin(), s.end());
-    reverse(s.begin(), s.end());
+    for(int i=0; n/=10; i++){
+        v.push_back(n%10);
+    }
     
-    answer = stoll(s);
+    for(int i = 1; i < v.size(); i ++){
+        for(int j = i; j > 0; j--){
+            if(v[j-1] < v[j]){
+                int temp = v[j];
+                v[j] = v[j-1];
+                v[j-1] = temp;
+            } 
+        }
+    }
+    
+    for(int i = 0; i < v.size(); i++){
+        answer = answer * 10 + v[i];
+    }
     
     return answer;
 }
+
+//long long solution(long long n){
+//    long long answer = 0;
+//    
+//    string s = to_string(n);
+//    sort(s.begin(), s.end());
+//    reverse(s.begin(), s.end());
+//    
+//    //sort(s.begin(), s.end(), greater<char>());
+//    
+//    answer = stoll(s);
+//    
+//    return answer;
+//}
