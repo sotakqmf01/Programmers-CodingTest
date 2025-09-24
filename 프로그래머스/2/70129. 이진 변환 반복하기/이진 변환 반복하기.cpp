a94d8717432dfb7532439fccbc110a4cce276bce
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 15:23:38 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.19 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -4,47 +4,72 @@
 
 using namespace std;
 
-vector<int> solution(string s) {
-    vector<int> answer;
-    int DeletedZeroCount = 0;
-    int TransCount = 0;
-    string NewStr = "";
-    
-    while(s != "1")
-    {
-        // 0 제거
-        NewStr.clear();
-        for(char c : s)
-        {
-            if(c == '1')
-            {
-                NewStr += c;
-            }
-            else
-            {
-                DeletedZeroCount++;
-            }
-        }
+//vector<int> solution(string s) {
+//    vector<int> answer;
+//    int DeletedZeroCount = 0;
+//    int TransCount = 0;
+//    string NewStr = "";
+//    
+//    while(s != "1")
+//    {
+//        // 0 제거
+//        NewStr.clear();
+//        for(char c : s)
+//        {
+//            if(c == '1')
+//            {
+//                NewStr += c;
+//            }
+//            else
+//            {
+//                DeletedZeroCount++;
+//            }
+//        }
+//
+//        // 0 제거 후 길이
+//        int len = NewStr.length();
+//        
+//        // 길이 이진 변환
+//        s.clear();
+//        while(len > 0)
+//        {
+//            s += len % 2 + '0';
+//            len /= 2;
+//        }
+//        // 이진 변환한게 어떻게 나오든 상관 없음
+//        // 어짜피 0을 제거할 것이기 때문
+//        //reverse(s.begin(), s.end());
+//        
+//        TransCount++;
+//    }
+//    
+//    answer.push_back(TransCount);
+//    answer.push_back(DeletedZeroCount);
+//    
+//    return answer;
+//}
 
-        // 0 제거 후 길이
-        int len = NewStr.length();
+vector<int> solution(string x){
+    vector<int> answer(2, 0);
+    int conversion = 0;
+    int zero = 0;
+    
+    while(x != "1"){
+        int currentZero = count(x.begin(), x.end(), '0');
+        zero += currentZero;
         
-        // 길이 이진 변환
-        s.clear();
-        while(len > 0)
-        {
-            s += len % 2 + '0';
-            len /= 2;
+        int length = x.length() - currentZero;
+        x.clear();
+        while(length > 0){
+            x += to_string(length % 2);
+            length /= 2;
         }
-        // 이진 변환한게 어떻게 나오든 상관 없음
-        // 어짜피 0을 제거할 것이기 때문
-        //reverse(s.begin(), s.end());
         
-        TransCount++;
+        conversion++;
     }
     
-    answer.push_back(TransCount);
-    answer.push_back(DeletedZeroCount);
+    answer[0] = conversion;
+    answer[1] = zero;
     
     return answer;
 }
