ebc39ab851e783273c6e8b965662e39aba012d59
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 20:44:17 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -23,19 +23,44 @@ using namespace std;
 //    return answer;
 //}
 
+// w * h = total
+// - w와 h는 total의 약수들임
+//  - 3부터 시작하는 이유 : 노란색 격자가 있으려면 2는 불가능
+//vector<int> solution(int brown, int yellow){
+//    vector<int> answer(2, 0);
+//    int total = brown + yellow;
+//    
+//    for(int i = 3; i*i <= total; i++){
+//        if(total % i == 0){
+//            if(2*(i + (total / i)) - 4 == brown){
+//                answer[0] = total / i;
+//                answer[1] = i;
+//                break;
+//            }
+//        }
+//    }
+//        
+//    return answer;
+//}
+
+// 다른 사람 풀이
+// yellow = (w-2) * (h-2)
+// brown = w * h - yellow = 2(w+h - 2)
+// => w + h = brown/2 + 2
 vector<int> solution(int brown, int yellow){
     vector<int> answer(2, 0);
-    int total = brown + yellow;
     
-    for(int i = 3; i*i <= total; i++){
-        if(total % i == 0){
-            if(2*(i + (total / i)) - 4 == brown){
-                answer[0] = total / i;
-                answer[1] = i;
-                break;
-            }
-        }
+    // 노란색 격자가 있으려면 h=3부터
+    int h = 3;
+    int w = (brown/2 + 2) - h;
+    
+    while(w * h != brown + yellow){
+        h++;
+        w--;
     }
-        
+    
+    answer[0] = w;
+    answer[1] = h;
+    
     return answer;
 }
