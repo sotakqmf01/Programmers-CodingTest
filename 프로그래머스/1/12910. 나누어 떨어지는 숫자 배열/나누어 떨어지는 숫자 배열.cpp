6bf016288fab86a39f623aa7b931fbbb0d4c6a29
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 18:45:31 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.38 ms, Memory: 6.04 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.cpp
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.cpp
@@ -1,28 +1,46 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<int> arr, int divisor) {
+//vector<int> solution(vector<int> arr, int divisor) {
+//    vector<int> answer;
+//    
+//    for(int i = 0; i < arr.size(); i++){
+//        if(arr[i] % divisor == 0)
+//            answer.push_back(arr[i]);
+//    }
+//    
+//    for(int i = 1; i < answer.size(); i++){
+//        for(int j = i; j > 0; j--){
+//            if(answer[j - 1] > answer[j]){
+//                int temp = answer[j];
+//                answer[j] = answer[j-1];
+//                answer[j-1] = temp;
+//            }
+//        }
+//    }
+//    
+//    if(answer.size() == 0)
+//        answer.push_back(-1);
+//    
+//    return answer;
+//}
+
+
+vector<int> solution(vector<int> arr, int divisor){
     vector<int> answer;
     
-    for(int i = 0; i < arr.size(); i++){
-        if(arr[i] % divisor == 0)
-            answer.push_back(arr[i]);
+    for(const int num : arr){
+        if(num % divisor == 0)
+            answer.emplace_back(num);
     }
     
-    for(int i = 1; i < answer.size(); i++){
-        for(int j = i; j > 0; j--){
-            if(answer[j - 1] > answer[j]){
-                int temp = answer[j];
-                answer[j] = answer[j-1];
-                answer[j-1] = temp;
-            }
-        }
-    }
+    if(answer.empty())
+        answer.emplace_back(-1);
     
-    if(answer.size() == 0)
-        answer.push_back(-1);
+    sort(answer.begin(), answer.end());
     
     return answer;
 }
