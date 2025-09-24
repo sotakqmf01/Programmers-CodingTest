37719fb66deb6a1737a11fb48d35899aa87994cb
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 14:45:49 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -1,31 +1,30 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
-#include <sstream>
-#include <algorithm>
 
 using namespace std;
 
-//vector<int> solution(long long n){
-//    vector<int> answer;
-//    
-//    while(n > 0){
-//        answer.emplace_back(n % 10);
-//        n /= 10;
-//    }
-//    
-//    return answer;
-//}
-
-// 다른 사람 풀이
 vector<int> solution(long long n){
     vector<int> answer;
-    string nStr = to_string(n);
-
-    for(int i = nStr.length() - 1; i >= 0; i--){
-        answer.emplace_back(nStr[i] - '0');
-        // answer.
+    
+    while(n > 0){
+        answer.emplace_back(n % 10);
+        n /= 10;
     }
     
     return answer;
 }
+
+// 문자열을 사용한 풀이
+//vector<int> solution(long long n){
+//    vector<int> answer;
+//    string nStr = to_string(n);
+//
+//    for(int i = nStr.length() - 1; i >= 0; i--){
+//        answer.emplace_back(nStr[i] - '0');
+//        //answer.emplace_back(stoi(nStr.substr(i, 1)));
+//        //answer.emplace_back(atoi(nStr.substr(i, 1).c_str()));
+//    }
+//    
+//    return answer;
+//}
