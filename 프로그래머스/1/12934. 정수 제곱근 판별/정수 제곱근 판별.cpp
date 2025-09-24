5242d0d8fa36c8ce649e9a62fa1c3c2c3bf511df
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 17:37:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -4,25 +4,28 @@
 
 using namespace std;
 
-//long long solution(long long n) {
-//    long long answer = 0;
+// 시간 초과
+// i를 int로 하면 i*i가 [int]라 최댓값(약 21.5억)을 넘으면, 오버플로우 발생
+// => 제한사항의 50조에 도달 못 함
+// => for문 종료 조건 만족 불가능 => 무한 루프
+//long long solution(long long n){
+//    long long answer = -1;
 //    
-//    if (n == (long long)sqrt(n) * (long long)sqrt(n))
-//        answer = (long long)pow((sqrt(n) + 1), 2);
-//    else
-//        answer = -1;
+//    for(int i = 1; i*i <= n; i++){
+//        if(i * i == n)
+//            answer = (i + 1) * (i + 1);
+//    }
 //    
 //    return answer;
 //}
 
-// 시간 초과
-long long solution(long long n){
-    long long answer = -1;
+long long solution(long long n) {
+    long long answer = 0;
     
-    for(long long i = 1; i*i <= n; i++){
-        if(i * i == n)
-            answer = (i + 1) * (i + 1);
-    }
+    if (n == (long long)sqrt(n) * (long long)sqrt(n))
+        answer = (long long)pow((sqrt(n) + 1), 2);
+    else
+        answer = -1;
     
     return answer;
 }
