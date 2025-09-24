e78b622ac165cc83d3f50734539d7d0c6dec45b2
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 18:39:29 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -4,37 +4,27 @@
 
 using namespace std;
 
-//string solution(vector<string> seoul) {
-//    string answer = "";
-//    int n = 0;
-//    
-//    for(int i = 0; i < seoul.size(); i++){
-//        if(seoul[i] == "Kim")
-//            n = i;
-//    }
-//    
-//    answer = "김서방은 " + to_string(n) + "에 있다";
-//    
-//    return answer;
-//}
-
-//string solution(vector<string> seoul){
-//    string answer = "";
-//    
-//    for(int i = 0; i < seoul.size(); i++){
-//        if(seoul[i] == "Kim")
-//            answer = "김서방은 " + to_string(i) + "에 있다";
-//    }
-//    
-//    return answer;
-//}
-
-string solution(vector<string> seoul){
+string solution(vector<string> seoul) {
     string answer = "";
+    int n = 0;
     
-    vector<string>::iterator it = find(seoul.begin(), seoul.end(), "Kim");
+    for(int i = 0; i < seoul.size(); i++){
+        if(seoul[i] == "Kim")
+            n = i;
+    }
     
-    answer = "김서방은 " + to_string(it - seoul.begin()) + "에 있다";
+    answer = "김서방은 " + to_string(n) + "에 있다";
     
     return answer;
 }
+
+// find() 사용
+//string solution(vector<string> seoul){
+//    string answer = "";
+//    
+//    vector<string>::iterator it = find(seoul.begin(), seoul.end(), "Kim");
+//    
+//    answer = "김서방은 " + to_string(it - seoul.begin()) + "에 있다";
+//    
+//    return answer;
+//}
