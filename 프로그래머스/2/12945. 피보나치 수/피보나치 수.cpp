2ecd7f787b1d7774171e11f5391d834af2b0873c
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 15:43:50 +0900

    [level 2] Title: 피보나치 수, Time: 1.36 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -3,26 +3,30 @@
 
 using namespace std;
 
-//vector<long long> Fibonacci = {0, 1};
-//
-//int solution(int n) {
-//    int answer = 0;
-//    
-//    for(int i = 0; i <= n - 2; i++)
-//    {
-//        Fibonacci.push_back((Fibonacci[i] + Fibonacci[i+1]) % 1234567);
-//    }
-//   
-//    return Fibonacci[n];
-//}
+// 나머지 연산 법칙
+// - (A + B) % C = (A % C + B % C) % C
+// => f(n) % 1234567 = (f(n-1) + f(n-2)) % 1234567
+//                   = (f(n-1) % 1234567 + f(n-2) % 1234567) % 1234567
+vector<long long> Fibonacci = {0, 1};
 
-int solution(int n){
-    vector<long long> fibonacci(n+1, 0);
-    fibonacci[1] = 1;
+int solution(int n) {
+    int answer = 0;
     
-    for(int i = 2; i <= n; i++){
-        fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]) % 1234567;
+    for(int i = 0; i <= n - 2; i++)
+    {
+        Fibonacci.push_back((Fibonacci[i] + Fibonacci[i+1]) % 1234567);
     }
-        
-    return fibonacci[n];
+   
+    return Fibonacci[n];
 }
+
+//int solution(int n){
+//    vector<long long> fibonacci(n+1, 0);
+//    fibonacci[1] = 1;
+//    
+//    for(int i = 2; i <= n; i++){
+//        fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]) % 1234567;
+//    }
+//        
+//    return fibonacci[n];
+//}
