09ed8661b713278692e94f4541dc7857f1443beb
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 18:21:18 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -3,47 +3,47 @@
 
 using namespace std;
 
-//int solution(int num) {
-//    if(num == 1)
-//        return 0;
-//    
-//    int answer = 0;
-//    long long n = num;
-//    
-//    while(n != 1){
-//        if(answer >= 500)
-//            return -1;
-//        
-//        if(n % 2 == 0)
-//            n = n / 2;
-//        else
-//            n = n*3 + 1;
-//        
-//        answer++;
-//    }
-//    
-//    return answer;
-//}
-
-
-int solution(int num){
+int solution(int num) {
+    if(num == 1)
+        return 0;
+    
     int answer = 0;
-    long long temp = num;
-    int count = 0;
+    long long n = num;
     
-    while(temp != 1){
-        if(temp % 2 == 0)
-            temp /= 2;
-        else
-            temp = temp * 3 + 1;
-        
-        count++;
-        if(count >= 500){
+    while(n != 1){
+        if(answer >= 500)
             return -1;
-        }
+        
+        if(n % 2 == 0)
+            n = n / 2;
+        else
+            n = n*3 + 1;
+        
+        answer++;
     }
-    
-    answer = count;
     
     return answer;
 }
+
+
+//int solution(int num){
+//    int answer = 0;
+//    long long temp = num;
+//    int count = 0;
+//    
+//    while(temp != 1){
+//        if(temp % 2 == 0)
+//            temp /= 2;
+//        else
+//            temp = temp * 3 + 1;
+//        
+//        count++;
+//        if(count >= 500){
+//            return -1;
+//        }
+//    }
+//    
+//    answer = count;
+//    
+//    return answer;
+//}
