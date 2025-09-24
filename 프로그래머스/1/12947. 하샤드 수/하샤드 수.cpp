bf775cf4f95a46648820608bd1aeb8aad9522758
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 17:49:31 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -3,19 +3,36 @@
 
 using namespace std;
 
-bool solution(int x) {
-    bool answer = true;
-    
+//bool solution(int x) {
+//    bool answer = true;
+//    
+//    int sum = 0;
+//    int n = x;
+//    
+//    while(n > 0){
+//        sum = sum + (n % 10);
+//        n = n / 10;
+//    }
+//    
+//    if((x % sum) != 0)
+//        answer = false;
+// 
+//    return answer;
+//}
+
+bool solution(int x){
+    bool answer = false;
+    int temp = x;
     int sum = 0;
-    int n = x;
     
-    while(n > 0){
-        sum = sum + (n % 10);
-        n = n / 10;
+    // 각 자리 수의 합
+    while(temp > 0){
+        sum += temp % 10;
+        temp /= 10;
     }
     
-    if((x % sum) != 0)
-        answer = false;
- 
+    if(x % sum == 0)
+        answer = true;
+    
     return answer;
 }
