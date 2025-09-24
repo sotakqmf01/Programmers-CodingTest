ee5ff768ec73b19230d864d5f772ddfa7297def5
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-24 18:51:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -3,16 +3,24 @@
 
 using namespace std;
 
-string solution(string phone_number) {
-    string answer = "";
-    int len = phone_number.length();
-    
-    for(int i = 0; i < len; i++){
-        if(i < len-4)
-            answer += '*';
-        else
-            answer += phone_number[i];
+//string solution(string phone_number) {
+//    string answer = "";
+//    int len = phone_number.length();
+//    
+//    for(int i = 0; i < len; i++){
+//        if(i < len-4)
+//            answer += '*';
+//        else
+//            answer += phone_number[i];
+//    }
+//    
+//    return answer;
+//}
+
+string solution(string phone_number){
+    for(int i = phone_number.length() - 5; i >= 0; i--){
+        phone_number[i] = '*';
     }
     
-    return answer;
+    return phone_number;
 }
