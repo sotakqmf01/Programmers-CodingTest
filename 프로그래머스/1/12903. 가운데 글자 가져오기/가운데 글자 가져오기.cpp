9861d96d671809b5d30d46b7f39a47a4d131125c
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-25 13:52:50 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -3,19 +3,31 @@
 
 using namespace std;
 
-string solution(string s) {
+//string solution(string s) {
+//    string answer = "";
+//    int len = s.length();
+//    
+//    if(len % 2 == 1){
+//        answer = s[len/2];
+//    }
+//    else{
+//        answer = s.substr(len/2 - 1, 2);
+//        //answer = s[len/2-1];
+//        //answer += s[len/2];
+//    }
+//    
+//    
+//    return answer;
+//}
+
+string solution(string s){
     string answer = "";
-    int len = s.length();
+    int length = s.length();
     
-    if(len % 2 == 1){
-        answer = s[len/2];
-    }
-    else{
-        answer = s.substr(len/2 - 1, 2);
-        //answer = s[len/2-1];
-        //answer += s[len/2];
-    }
-    
+    if(length % 2 == 0)
+        answer = s.substr((length / 2) - 1, 2);
+    else
+        answer = s.substr(length / 2, 1);
     
     return answer;
 }
