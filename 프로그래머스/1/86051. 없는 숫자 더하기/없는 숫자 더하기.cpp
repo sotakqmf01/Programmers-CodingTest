2d75dfbbea095bd6baf78b47b785f94874b3e2e1
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-25 13:09:21 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -3,16 +3,33 @@
 
 using namespace std;
 
-int solution(vector<int> numbers) {
+//int solution(vector<int> numbers) {
+//    int answer = 0;
+//    int compare[10] = {1,1,1,1,1,1,1,1,1,1};
+//    
+//    for(int i = 0; i<numbers.size(); i++){
+//        compare[numbers[i]] = 0;
+//    }
+//    
+//    for(int i = 0; i<10; i++){
+//        if(compare[i] != 0)
+//            answer += i;
+//    }
+//    
+//    return answer;
+//}
+
+
+int solution(vector<int> numbers){
     int answer = 0;
-    int compare[10] = {1,1,1,1,1,1,1,1,1,1};
+    vector<bool> check(10, false);
     
-    for(int i = 0; i<numbers.size(); i++){
-        compare[numbers[i]] = 0;
+    for(const int num : numbers){
+        check[num] = true;
     }
     
-    for(int i = 0; i<10; i++){
-        if(compare[i] != 0)
+    for(int i = 0; i < check.size(); i++){
+        if(check[i] == false)
             answer += i;
     }
     
