0a014feadd6d54ca227d9dd2676cded09e16b30d
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-25 15:34:33 +0900

    [level 2] Title: 귤 고르기, Time: 33.61 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -5,41 +5,67 @@
 
 using namespace std;
 
-int solution(int k, vector<int> tangerine) {
+//int solution(int k, vector<int> tangerine) {
+//    int answer = 0;
+//    int Count = 0;
+//    map<int, int> m;
+//    priority_queue<int> pq;
+//    
+//    //종류 별 개수
+//    for(int i = 0; i<tangerine.size(); i++)
+//    {
+//        if(m.find(tangerine[i]) == m.end())
+//        {
+//            m.insert(make_pair(tangerine[i], 1));
+//        }
+//        else
+//        {
+//            m[tangerine[i]]++;
+//        }
+//    }
+//    
+//    // 종류 별 개수 정렬
+//    for(pair<int, int> CountByType : m)
+//    {
+//        pq.push(CountByType.second);
+//    }
+//    
+//    // k개를 고를 때 종류의 수를 가장 적게 만들기
+//    while(!pq.empty())
+//    {
+//        Count += pq.top();
+//        answer++;
+//        
+//        if(Count >= k)
+//            return answer;
+//        
+//        pq.pop();
+//    }
+//    
+//    return answer;
+//}
+
+
+int solution(int k, vector<int> tangerine){
     int answer = 0;
-    int Count = 0;
-    map<int, int> m;
-    priority_queue<int> pq;
     
-    //종류 별 개수
-    for(int i = 0; i<tangerine.size(); i++)
-    {
-        if(m.find(tangerine[i]) == m.end())
-        {
-            m.insert(make_pair(tangerine[i], 1));
-        }
-        else
-        {
-            m[tangerine[i]]++;
-        }
+    map<int, int> countByType;
+    priority_queue<int> pqForCount;
+    
+    for(const int& size : tangerine){
+        countByType[size]++;
     }
     
-    // 종류 별 개수 정렬
-    for(pair<int, int> CountByType : m)
-    {
-        pq.push(CountByType.second);
+    for(const pair<int, int>& count : countByType){
+        pqForCount.push(count.second);
     }
     
-    // k개를 고를 때 종류의 수를 가장 적게 만들기
-    while(!pq.empty())
-    {
-        Count += pq.top();
+    int total = 0;
+    while(total < k){
+        total += pqForCount.top();
+        pqForCount.pop();
+        
         answer++;
-        
-        if(Count >= k)
-            return answer;
-        
-        pq.pop();
     }
     
     return answer;
