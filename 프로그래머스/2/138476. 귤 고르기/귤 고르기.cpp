5238bd59f4768add52bf1411e6c51c4e172b2e29
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-25 15:44:36 +0900

    [level 2] Title: 귤 고르기, Time: 36.28 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -48,49 +48,49 @@ using namespace std;
 
 
 // map + priority_queue
-//int solution(int k, vector<int> tangerine){
-//    int answer = 0;
-//    
-//    map<int, int> countBySize;
-//    priority_queue<int> pqForCount;
-//    
-//    for(const int& size : tangerine){
-//        countBySize[size]++;
-//    }
-//    
-//    for(const pair<int, int>& count : countBySize){
-//        pqForCount.push(count.second);
-//    }
-//    
-//    int total = 0;
-//    while(total < k){
-//        total += pqForCount.top();
-//        pqForCount.pop();
-//        
-//        answer++;
-//    }
-//    
-//    return answer;
-//}
-
-// 다른 사람 풀이 : vector만 사용
 int solution(int k, vector<int> tangerine){
     int answer = 0;
     
-    int maxSize = *max_element(tangerine.begin(), tangerine.end());
-    vector<int> countBySize(maxSize + 1, 0);
+    map<int, int> countBySize;
+    priority_queue<int> pqForCount;
     
     for(const int& size : tangerine){
         countBySize[size]++;
     }
     
-    sort(countBySize.begin(), countBySize.end(), greater<int>());
+    for(const pair<int, int>& count : countBySize){
+        pqForCount.push(count.second);
+    }
     
     int total = 0;
     while(total < k){
-        total += countBySize[answer];
+        total += pqForCount.top();
+        pqForCount.pop();
+        
         answer++;
     }
     
     return answer;
 }
+
+// 다른 사람 풀이 : vector만 사용
+//int solution(int k, vector<int> tangerine){
+//    int answer = 0;
+//    
+//    int maxSize = *max_element(tangerine.begin(), tangerine.end());
+//    vector<int> countBySize(maxSize + 1, 0);
+//    
+//    for(const int& size : tangerine){
+//        countBySize[size]++;
+//    }
+//    
+//    sort(countBySize.begin(), countBySize.end(), greater<int>());
+//    
+//    int total = 0;
+//    while(total < k){
+//        total += countBySize[answer];
+//        answer++;
+//    }
+//    
+//    return answer;
+//}
