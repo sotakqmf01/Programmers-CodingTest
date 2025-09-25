5f190e2f4c5b0b259a802d864ccbd69406622c2e
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-25 14:25:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -4,24 +4,42 @@
 using namespace std;
 
 // 비트 연산자 사용해야하면 어떻게 할지 생각해보기
-int solution(int n, int a, int b)
-{
-    int Round = 1;
+//int solution(int n, int a, int b)
+//{
+//    int Round = 1;
+//
+//    while(Round <= n)
+//    {
+//        if((a+1)/2 == (b+1)/2)
+//        {
+//            break;
+//        }
+//        else
+//        {
+//            a = (a+1)/2;
+//            b = (b+1)/2;
+//        }
+//
+//        Round++;
+//    }
+//
+//    return Round;
+//}
 
-    while(Round <= n)
-    {
-        if((a+1)/2 == (b+1)/2)
-        {
+
+int solution(int n, int a, int b){
+    int answer = 1;
+
+    while(1){
+        a = (a - 1) / 2 + 1;
+        b = (b - 1) / 2 + 1;
+        
+        // 서로 붙게 된다면, 둘 중 이겼을 때 다음 번호는 같음
+        if(a == b)
             break;
-        }
-        else
-        {
-            a = (a+1)/2;
-            b = (b+1)/2;
-        }
-
-        Round++;
+        
+        answer++;
     }
-
-    return Round;
+    
+    return answer;
 }
