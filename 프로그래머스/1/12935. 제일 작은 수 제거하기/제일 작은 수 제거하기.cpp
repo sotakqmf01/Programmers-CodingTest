5f1d57abef3836d9153fac40613b0a398c47edb7
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-25 13:32:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 10.56 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -1,27 +1,38 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<int> arr) {
-    vector<int> answer;
-    int size = arr.size();
-    vector<int>::iterator minit = arr.begin();
-    int min = *minit;
+//vector<int> solution(vector<int> arr) {
+//    vector<int> answer;
+//    int size = arr.size();
+//    vector<int>::iterator minit = arr.begin();
+//    int min = *minit;
+//    
+//    if(size == 1){
+//        answer.push_back(-1);
+//    }
+//    else{
+//        for(vector<int>::iterator it = arr.begin()+1; it != arr.end(); it++){
+//            if( min > *(it) ){
+//                min = *(it);
+//                minit = it;
+//            }
+//        }
+//        arr.erase(minit);
+//        answer = arr;
+//    }
+//    
+//    return answer;
+//}
+
+vector<int> solution(vector<int> arr){
+    vector<int>::iterator it = min_element(arr.begin(), arr.end());
+    arr.erase(it);
     
-    if(size == 1){
-        answer.push_back(-1);
-    }
-    else{
-        for(vector<int>::iterator it = arr.begin()+1; it != arr.end(); it++){
-            if( min > *(it) ){
-                min = *(it);
-                minit = it;
-            }
-        }
-        arr.erase(minit);
-        answer = arr;
-    }
+    if(arr.empty())
+        arr.emplace_back(-1);
     
-    return answer;
+    return arr;
 }
