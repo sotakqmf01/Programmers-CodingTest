c318f7c40a3d71a6538085141f6b2e61b6b16eea
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-25 14:45:39 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -4,28 +4,47 @@
 
 using namespace std;
 
-int GetGCD(int a, int b)
-{
-    if(b == 0)  return a;
+//int GetGCD(int a, int b)
+//{
+//    if(b == 0)  return a;
+//    
+//    return GetGCD(b, a%b);
+//}
+//
+//int solution(vector<int> arr) {    
+//    sort(arr.begin(), arr.end(), greater<int>());
+//    
+//    int num = arr[0];
+//    int gcd = 0;
+//    int lcm = 0;
+//
+//    // lcm(a,b) = a * b / gcd(a,b);
+//    // lcm(a,b,c) = lcm(lcm(a,b),c) = lcm(a,b) * c / gcd(lcm(a,b),c)
+//    for(int i = 1; i<arr.size(); i++)
+//    {
+//        gcd = GetGCD(num, arr[i]);
+//        lcm = num * arr[i] / gcd;
+//        num = lcm;
+//    }
+//    
+//    return num;
+//}
+
+int getGCD(int a, int b){
+    if(b == 0)
+        return a;
     
-    return GetGCD(b, a%b);
+    return getGCD(b, a % b);
 }
 
-int solution(vector<int> arr) {    
-    sort(arr.begin(), arr.end(), greater<int>());
+int solution(vector<int> arr){
+    int answer = 0;
+    int a = arr[0];
     
-    int num = arr[0];
-    int gcd = 0;
-    int lcm = 0;
-
-    // lcm(a,b) = a * b / gcd(a,b);
-    // lcm(a,b,c) = lcm(lcm(a,b),c) = lcm(a,b) * c / gcd(lcm(a,b),c)
-    for(int i = 1; i<arr.size(); i++)
-    {
-        gcd = GetGCD(num, arr[i]);
-        lcm = num * arr[i] / gcd;
-        num = lcm;
+    for(int i = 1; i < arr.size(); i++){
+        answer = a * arr[i] / getGCD(a, arr[i]);
+        a = answer;
     }
     
-    return num;
+    return answer;
 }
