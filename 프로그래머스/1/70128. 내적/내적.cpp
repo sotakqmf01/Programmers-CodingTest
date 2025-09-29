f779160581ef1cc4f796fb0d2782151dc79f03ba
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-29 10:16:26 +0900

    [level 1] Title: 내적, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.cpp
+++ 프로그래머스/1/70128. 내적/내적.cpp
@@ -3,11 +3,22 @@
 
 using namespace std;
 
-int solution(vector<int> a, vector<int> b) {
+//int solution(vector<int> a, vector<int> b) {
+//    int answer = 0;
+//    
+//    for(int i = 0; i<a.size(); i++){
+//        answer += a[i]*b[i];
+//    }
+//    
+//    return answer;
+//}
+
+
+int solution(vector<int> a, vector<int> b){
     int answer = 0;
     
-    for(int i = 0; i<a.size(); i++){
-        answer += a[i]*b[i];
+    for(int i = 0; i < a.size(); i++){
+        answer += a[i] * b[i];
     }
     
     return answer;
