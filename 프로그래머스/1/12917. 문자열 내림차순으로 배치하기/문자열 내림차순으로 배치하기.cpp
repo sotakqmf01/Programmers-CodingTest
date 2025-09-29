5b01b3b46ab6e6af5c316be699a0154f6dc4d70f
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-29 10:35:36 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
@@ -1,18 +1,28 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 using namespace std;
 
-bool compare(const char& a, const char& b){
-    return a > b;
-}
+//bool compare(const char& a, const char& b){
+//    return a > b;
+//}
+//
+//string solution(string s) {
+//    string answer = "";
+//    
+//    sort(s.begin(), s.end(), compare);
+//    answer = s;
+//    
+//    return answer;
+//}
 
-string solution(string s) {
-    string answer = "";
+
+string solution(string s){
+    string answer = s;
     
-    sort(s.begin(), s.end(), compare);
-    answer = s;
+    sort(answer.begin(), answer.end(), greater<char>());
     
     return answer;
 }
