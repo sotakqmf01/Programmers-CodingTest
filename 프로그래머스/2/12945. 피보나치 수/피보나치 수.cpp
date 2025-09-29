4218ec0f84eb8340bcbc0d1838a03e0de248cd86
sotakqmf01 <57822478+sotakqmf01@users.noreply.github.com>
2025-09-29 15:40:05 +0900

    [level 2] Title: 피보나치 수, Time: 1.26 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -3,10 +3,7 @@
 
 using namespace std;
 
-// 나머지 연산 법칙
-// - (A + B) % C = (A % C + B % C) % C
-// => f(n) % 1234567 = (f(n-1) + f(n-2)) % 1234567
-//                   = (f(n-1) % 1234567 + f(n-2) % 1234567) % 1234567
+// 기저 사례
 vector<long long> Fibonacci = {0, 1};
 
 int solution(int n) {
@@ -14,6 +11,10 @@ int solution(int n) {
     
     for(int i = 0; i <= n - 2; i++)
     {
+        // 나머지 연산 법칙
+        // - (A + B) % C = (A % C + B % C) % C
+        // => f(n) % 1234567 = (f(n-1) + f(n-2)) % 1234567
+        //                   = (f(n-1) % 1234567 + f(n-2) % 1234567) % 1234567
         Fibonacci.push_back((Fibonacci[i] + Fibonacci[i+1]) % 1234567);
     }
    
